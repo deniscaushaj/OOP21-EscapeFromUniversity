700b1c2e0cfda99c42770a6281b6c81cea8804ea
Emanuele Bertolero <emanuele.bertolero@studio.unibo.it>
2022-04-26 16:58:02 +0200

    Created interface for GameObject

---
+++ src/main/java/escapefromuniversity/model/GameObject.java
@@ -0,0 +1,11 @@
+package escapefromuniversity.model;
+
+public interface GameObject {
+	
+	Point2D getObjectPosition();
+	
+	int getID();
+	
+	void setID(int ID);
+	
+}
---
+++ src/main/java/escapefromuniversity/model/Point2D.java
@@ -0,0 +1,21 @@
+package escapefromuniversity.model;
+
+public class Point2D {
+	
+	private final double x;
+	private final double y;
+	
+	 public Point2D(final double x, final double y) {
+		 this.x = x;
+		 this.y = y;
+	 }
+	
+	public double getX() {
+		return x;
+	}
+	
+	public double getY() {
+		return y;
+	}
+	
+}
