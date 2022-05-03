c35e54b3d373cce3d8edf17f3adb5f6e45cd3de8
Daniel Capannini <daniel12capannini@gmail.com>
2022-05-03 15:56:38 +0200

    implementation Vector2D

--- src/main/java/escapefromuniversity/model/Vector2D.java
+++ src/main/java/escapefromuniversity/model/Vector2D.java
@@ -1,5 +1,21 @@
 package escapefromuniversity.model;
 
 public class Vector2D {
+	
+	private final double x;
+	private final double y;
+	
+	 public Vector2D(final double x, final double y) {
+		 this.x = x;
+		 this.y = y;
+	 }
+	
+	public double getX() {
+		return x;
+	}
+	
+	public double getY() {
+		return y;
+	}
 
 }
