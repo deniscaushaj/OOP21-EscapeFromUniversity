f7133add576a671d866cc8c6cc7b2219281b558d
Daniel Capannini <daniel12capannini@gmail.com>
2022-05-03 16:09:58 +0200

    create adstract class AbstractDynamicGameObject

---
+++ src/main/java/escapefromuniversity/model/AbstractDynamicGameObject.java
@@ -0,0 +1,41 @@
+package escapefromuniversity.model;
+
+public abstract class AbstractDynamicGameObject implements DynamicGameObject {
+
+	@Override
+	public Point2D getObjectPosition() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getID() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setID(int ID) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getSpeed() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Vector2D getDyrection() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setDyrectino(Vector2D newDyrection) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
