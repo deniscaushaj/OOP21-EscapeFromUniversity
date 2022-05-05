24c56eea1e13db293cbce2e9ee4234e334282e79
Daniel Capannini <daniel12capannini@gmail.com>
2022-05-05 16:11:56 +0200

    correct interface bullet

--- src/main/java/escapefromuniversity/model/bullect/Bullet.java
+++ src/main/java/escapefromuniversity/model/bullect/Bullet.java
@@ -2,8 +2,18 @@ package escapefromuniversity.model.bullect;
 
 import escapefromuniversity.model.DynamicGameObject;
 
+
+/**
+ * 
+ * interface bullet as extension of DynamicGameObject.
+ *
+ */
 public interface Bullet extends DynamicGameObject{
 	
-	
+	/**
+	 * 
+	 * @return the bullet damage.
+	 */
+	int getDamage();
 
 }
