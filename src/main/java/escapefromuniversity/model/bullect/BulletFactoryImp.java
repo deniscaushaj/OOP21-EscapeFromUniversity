6ab92176bcbc3b6b6fd661401a2027c9d509cf57
Daniel Capannini <daniel12capannini@gmail.com>
2022-05-06 10:39:08 +0200

    create class BulletFactoryImp

---
+++ src/main/java/escapefromuniversity/model/bullect/BulletFactoryImp.java
@@ -0,0 +1,50 @@
+package escapefromuniversity.model.bullect;
+
+import escapefromuniversity.model.Point2D;
+import escapefromuniversity.model.Vector2D;
+
+public class BulletFactoryImp implements BulletFactory{
+
+	@Override
+	public Bullet createProtagonistBullet(Point2D startPosition, Vector2D Diration) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Bullet createBoss1Bullet(Point2D startPosition, Vector2D Diration) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Bullet createBoss2Bullet(Point2D startPosition, Vector2D Diration) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Bullet createBoss3Bullet(Point2D startPosition, Vector2D Diration) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Bullet createBoss4Bullet(Point2D startPosition, Vector2D Diration) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Bullet createBoss5Bullet(Point2D startPosition, Vector2D Diration) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Bullet createBoss6Bullet(Point2D startPosition, Vector2D Diration) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
