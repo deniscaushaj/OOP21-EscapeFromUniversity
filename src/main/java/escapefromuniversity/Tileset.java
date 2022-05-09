d0a16033bf881bffc706175a41d3b265026bff3e
Denis Caushaj <denis.caushaj@studio.unibo.it>
2022-05-09 17:24:14 +0200

    Add Tileset class

---
+++ src/main/java/escapefromuniversity/Tileset.java
@@ -0,0 +1,13 @@
+package escapefromuniversity;
+
+public class Tileset {
+    private final String fileName;
+
+    public Tileset(final String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileName() {
+        return this.fileName;
+    }
+}
