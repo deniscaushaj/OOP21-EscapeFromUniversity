3d4be73da1f50b06dc26cc4e8aeaeeb2744b16c2
Denis Caushaj <denis.caushaj@studio.unibo.it>
2022-05-10 15:10:30 +0200

    Add Tileset class

--- src/main/java/escapefromuniversity/Tileset.java
+++ src/main/java/escapefromuniversity/Tileset.java
@@ -2,12 +2,30 @@ package escapefromuniversity;
 
 public class Tileset {
     private final String fileName;
+    private final int tilesCount;
+    private final int columns;
+    private final int rows;
 
-    public Tileset(final String fileName) {
+    public Tileset(final String fileName, int tilesCount, int columns) {
         this.fileName = fileName;
+        this.tilesCount = tilesCount;
+        this.columns = columns;
+        this.rows = tilesCount / columns;
     }
 
     public String getFileName() {
         return this.fileName;
+    }
+
+    public int getTilesCount() {
+        return this.tilesCount;
+    }
+
+    public int getColumns() {
+        return this.columns;
+    }
+
+    public int getRows() {
+        return this.rows;
     }
 }
