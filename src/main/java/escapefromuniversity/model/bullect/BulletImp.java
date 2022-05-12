0248287e2f0591631ee89ad0430da1e373a29e9a
Daniel Capannini <daniel12capannini@gmail.com>
2022-05-12 16:33:18 +0200

    in BulletImp implement collisionWith

--- src/main/java/escapefromuniversity/model/bullect/BulletImp.java
+++ src/main/java/escapefromuniversity/model/bullect/BulletImp.java
@@ -1,10 +1,12 @@
 package escapefromuniversity.model.bullect;
 
 import escapefromuniversity.model.AbstractDynamicGameObject;
+import escapefromuniversity.model.GameCollisionType;
 import escapefromuniversity.model.GameObject;
 import escapefromuniversity.model.GameObjectType;
 import escapefromuniversity.model.Point2D;
 import escapefromuniversity.model.Vector2D;
+import escapefromuniversity.model.enemy.Enemy;
 
 public class BulletImp extends AbstractDynamicGameObject implements Bullet{
 	
@@ -24,6 +26,49 @@ public class BulletImp extends AbstractDynamicGameObject implements Bullet{
 	@Override
 	public void update(double deltaTime) {
 		this.move(deltaTime);
+		
+	}
+
+	@Override
+	public void collisionWith(GameObject gObj2) {
+		if(this.collisionWithCont(gObj2)) {
+			return;
+		}
+		switch(gObj2.getType().getCollisionType()) {
+		case OBSTACLE:
+			//delete this object
+			break;
+		case BULLET:
+			if(this.getType().getCollisionType().equals(GameCollisionType.IMMUNE_BULLET)) {
+				//delete gObj2
+			}
+			break;
+		case IMMUNE_BULLET:
+			if(this.getType().getCollisionType().equals(GameCollisionType.BULLET)) {
+				//delete this object
+			}
+			break;
+		case ENTITY:
+			if(gObj2.getType().equals(GameObjectType.PLAYER) && !this.getType().equals(GameObjectType.BULLET_PROTAGINIST)) {
+				//fare danno al protagonista
+				//delete this object
+			}else if(!gObj2.getType().equals(GameObjectType.PLAYER) && 
+					 this.getType().equals(GameObjectType.BULLET_PROTAGINIST)) {
+				final Enemy enemy = (Enemy) gObj2;
+				enemy.takeDamage(this.getDamage());
+				//delete this object
+			}
+			break;
+		case IMMUNE_ENTITY:
+			if(gObj2.getType().equals(GameObjectType.PLAYER) && !this.getType().equals(GameObjectType.BULLET_PROTAGINIST)) {
+				//delete this object
+			}else if(!gObj2.getType().equals(GameObjectType.PLAYER) && 
+					 this.getType().equals(GameObjectType.BULLET_PROTAGINIST)) {
+				//delete this object
+			}
+		default:
+			break;
+		}
 		
 	}
 
