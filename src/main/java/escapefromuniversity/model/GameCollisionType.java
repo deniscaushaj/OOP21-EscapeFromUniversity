65e00afc96cb4b218b2bea645d3675d2c0fba9bf
Daniel Capannini <daniel12capannini@gmail.com>
2022-05-12 15:14:24 +0200

    create enum GameCollisionType

---
+++ src/main/java/escapefromuniversity/model/GameCollisionType.java
@@ -0,0 +1,27 @@
+package escapefromuniversity.model;
+
+public enum GameCollisionType {
+	
+	/**
+	 * identifies a obstacle object.
+	 */
+	OBSTACLE,
+	/**
+	 * identifies a boss or protagonist.
+	 */
+	ENTITY,
+	/**
+	 * identifies a immune boss or protagonist.
+	 */
+	IMMUNE_ENTITY,
+	/**
+	 * identifies a bullet
+	 */
+	BULLET,
+	/**
+	 * identifies a immune bullet
+	 */
+	IMMUNE_BULLET;
+	
+
+}
