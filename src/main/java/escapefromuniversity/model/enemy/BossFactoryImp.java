3d7819b46d7178b2e9e7377ecdb04011f78a37f9
Daniel Capannini <daniel12capannini@gmail.com>
2022-05-14 16:06:58 +0200

    create class BossFactoryImp

---
+++ src/main/java/escapefromuniversity/model/enemy/BossFactoryImp.java
@@ -0,0 +1,51 @@
+package escapefromuniversity.model.enemy;
+
+import escapefromuniversity.model.GameObjectType;
+import escapefromuniversity.model.Point2D;
+import escapefromuniversity.model.Vector2D;
+
+public class BossFactoryImp implements BossFactory{
+
+	@Override
+	public Enemy createBoss1(int speed, Point2D position, Point2D upperCorner, Vector2D direction, GameObjectType type,
+			int life, long shootDelay, int impatDamage) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Enemy createBoss2(int speed, Point2D position, Point2D upperCorner, Vector2D direction, GameObjectType type,
+			int life, long shootDelay, int impatDamage) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Enemy createBoss3(int speed, Point2D position, Point2D upperCorner, Vector2D direction, GameObjectType type,
+			int life, long shootDelay, int impatDamage) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Enemy createBoss4(int speed, Point2D position, Point2D upperCorner, Vector2D direction, GameObjectType type,
+			int life, long shootDelay, int impatDamage) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Enemy createBoss5(int speed, Point2D position, Point2D upperCorner, Vector2D direction, GameObjectType type,
+			int life, long shootDelay, int impatDamage) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Enemy createBoss6(int speed, Point2D position, Point2D upperCorner, Vector2D direction, GameObjectType type,
+			int life, long shootDelay, int impatDamage) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
