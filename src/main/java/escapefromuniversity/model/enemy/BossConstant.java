e99d8d10d951f5a6e4b74b177871a290b0dd1070
Daniel Capannini <daniel12capannini@gmail.com>
2022-05-14 16:26:16 +0200

    create class BossConstant

---
+++ src/main/java/escapefromuniversity/model/enemy/BossConstant.java
@@ -0,0 +1,19 @@
+package escapefromuniversity.model.enemy;
+
+public class BossConstant {
+	
+	protected static final int boss1_life = 0;
+	protected static final int boss2_life = 0;
+	protected static final int boss3_life = 0;
+	protected static final int boss4_life = 0;
+	protected static final int boss5_life = 0;
+	protected static final int boss6_life = 0;
+	
+	protected static final int boss1_damage = 0;
+	protected static final int boss2_damage = 0;
+	protected static final int boss3_damage = 0;
+	protected static final int boss4_damage = 0;
+	protected static final int boss5_damage = 0;
+	protected static final int boss6_damage = 0;
+
+}
