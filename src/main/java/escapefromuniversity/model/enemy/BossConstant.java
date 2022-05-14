4d89e7dc4243653a3f8418c6a393dedaaefb338e
Daniel Capannini <daniel12capannini@gmail.com>
2022-05-14 16:29:49 +0200

    add costant in BossCostant

--- src/main/java/escapefromuniversity/model/enemy/BossConstant.java
+++ src/main/java/escapefromuniversity/model/enemy/BossConstant.java
@@ -15,5 +15,19 @@ public class BossConstant {
 	protected static final int boss4_damage = 0;
 	protected static final int boss5_damage = 0;
 	protected static final int boss6_damage = 0;
+	
+	protected static final int boss1_shootDelay = 0;
+	protected static final int boss2_shootDelay = 0;
+	protected static final int boss3_shootDelay = 0;
+	protected static final int boss4_shootDelay = 0;
+	protected static final int boss5_shootDelay = 0;
+	protected static final int boss6_shootDelay = 0;
+	
+	protected static final int boss1_speed = 0;
+	protected static final int boss2_speed = 0;
+	protected static final int boss3_speed = 0;
+	protected static final int boss4_speed = 0;
+	protected static final int boss5_speed = 0;
+	protected static final int boss6_speed = 0;
 
 }
