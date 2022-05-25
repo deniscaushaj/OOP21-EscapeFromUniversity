2dff3308b8b3dbfedce863dbe216e3095b9c0c92
Denis Caushaj <denis.caushaj@studio.unibo.it>
2022-05-25 15:35:17 +0200

    Create class Room

---
+++ src/main/java/escapefromuniversity/model/map/RoomImpl.java
@@ -0,0 +1,51 @@
+package escapefromuniversity.model.map;
+
+import escapefromuniversity.model.DynamicGameObject;
+import escapefromuniversity.model.GameObject;
+import escapefromuniversity.model.StaticGameObject;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class RoomImpl implements Room {
+    private final List<StaticGameObject> staticGameObjects = new LinkedList<>();
+    private final List<DynamicGameObject> dynamicGameObjects = new LinkedList<>();
+
+    @Override
+    public void update(double deltaTime) {
+        final List<DynamicGameObject> listDGObj = new LinkedList<>(List.copyOf(this.dynamicGameObjects));
+        listDGObj.iterator().forEachRemaining(obj -> { obj.update(deltaTime);});
+        listDGObj.iterator().forEachRemaining(obj -> {
+            for(final GameObject obj2 : this.getAllGameObject()) {
+                obj.collisionWith(obj2);
+            }
+        });
+    }
+
+    @Override
+    public void deleteGameObject(GameObject gameObject) {
+        staticGameObjects.remove(gameObject);
+        dynamicGameObjects.remove(gameObject);
+    }
+
+
+    @Override
+    public void addDynamicGameObject(DynamicGameObject dGObj) {
+        dynamicGameObjects.add(dGObj);
+
+    }
+
+    @Override
+    public void addStaticGameObject(StaticGameObject sGObj) {
+        staticGameObjects.add(sGObj);
+
+    }
+
+    @Override
+    public List<GameObject> getAllGameObject() {
+        final List<GameObject> gameObjects = new LinkedList<>();
+        gameObjects.addAll(this.dynamicGameObjects);
+        gameObjects.addAll(this.staticGameObjects);
+        return gameObjects;
+    }
+}
