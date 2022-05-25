dda85ac50cc55fef514ba12bdaffe8b0b686e010
Denis Caushaj <denis.caushaj@studio.unibo.it>
2022-05-25 15:35:48 +0200

    Create class TileDrawer

--- src/main/java/escapefromuniversity/model/map/TileDrawerImpl.java
+++ src/main/java/escapefromuniversity/model/map/TileDrawerImpl.java
@@ -1,5 +1,33 @@
 package escapefromuniversity.model.map;
 
-public class TileDrawerImpl {
+import javafx.util.Pair;
 
+public class TileDrawerImpl implements TileDrawer {
+
+    @Override
+    public void drawTileByID(int id, Pair<Integer, Integer> pos) {
+        var ts = this.searchTileset(id);
+        var tPos = calcTPos(id);
+    }
+
+    private Pair<Integer, Integer> calcTPos(int id) {
+        int x;
+        int y;
+        /*
+         * Per calcolare la posizione del tile che mi serve devo fare la get delle colonne e delle righe del tileset giusto.
+         * In questo modo posso determinare in base all'id la posizione.
+         * Es:
+         * ID = 50; ROWS = 7; COLS = 25
+         * POS = {1, 24}
+         * */
+        return new Pair<>(0, 0);
+    }
+
+    @Override
+    public Tileset searchTileset(int id) {
+        /*
+        * Voglio prendere da MapProperties la lista di Tileset per poter cercare quello che contiene l'id di cui ho bisogno
+        * */
+        return null;
+    }
 }
