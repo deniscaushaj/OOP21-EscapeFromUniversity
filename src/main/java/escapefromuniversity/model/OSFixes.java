96e568f0f5fcdc644bcd5dad2128f01c98acb37e
MarcoAntolini <marco.antolini2001@gmail.com>
2022-06-01 17:41:15 +0200

    add javadoc

--- src/main/java/escapefromuniversity/model/OSFixes.java
+++ src/main/java/escapefromuniversity/model/OSFixes.java
@@ -2,28 +2,34 @@ package escapefromuniversity.model;
 
 import java.io.IOException;
 
+/**
+ * Implements fields and methods to open web urls and filepaths working on every OS supported.
+ *
+ * @author marco
+ */
 public class OSFixes {
 	
 	public final static String FS = System.getProperty("file.separator");
 	public final static String UD = System.getProperty("user.dir");
 	private final static String OS = System.getProperty("os.name").toLowerCase();
 	
+	/**
+	 * Opens the url passed as a string.
+	 * @param url : the web url to open.
+	 */
 	public void openUrl(String url) {
-		// Windows:
 		if (OS.contains("win")) {
 			try {
 				new ProcessBuilder("rundll32 url.dll,FileProtocolHandler ", url).start();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-		// MacOs:
 		} else if (OS.contains("mac")) {
 			try {
 				new ProcessBuilder("open ", url).start();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-		// Linux:
 		} else if (OS.contains("nix") || OS.contains("nux")) {
 			try {
 				new ProcessBuilder("xdg-open ", url).start();
@@ -33,6 +39,12 @@ public class OSFixes {
 		}
 	}
 	
+	/**
+	 * Getter method to obtain a string which contains the filepath of the file to open from resources folder. 
+	 * @param folderName : the name of the subfolder inside resources folder to look into.
+	 * @param fileName : the name of the file to open.
+	 * @return a string containing the filepath.
+	 */
 	public static String getLocation(String folderName, String fileName) {
 		String resLocation = new String(UD + FS + "src" + FS + "main" + FS + "resources" + FS + folderName + FS + fileName);
 		return resLocation;
