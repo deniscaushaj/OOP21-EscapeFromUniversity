edf5a7fce9b0df9e26a212038323e7bc4a13586e
Daniel Capannini <daniel12capannini@gmail.com>
2022-06-03 15:15:00 +0200

    create new class GameModelImp

---
+++ src/main/java/escapefromuniversity/model/GameModelImp.java
@@ -0,0 +1,33 @@
+package escapefromuniversity.model;
+
+import java.util.List;
+
+import escapefromuniversity.model.gameObject.GameObject;
+
+public class GameModelImp implements GameModel{
+
+	@Override
+	public List<GameObject> getAllGameObj() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateGame(double deltaTime) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isWin() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isLost() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
