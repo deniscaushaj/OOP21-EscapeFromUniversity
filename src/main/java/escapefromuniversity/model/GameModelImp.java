d30c49b3e528818d6e5a1b7bf70685a3fd9830c1
Daniel Capannini <daniel12capannini@gmail.com>
2022-06-03 15:38:53 +0200

    in gameModelImp add javaDoc

--- src/main/java/escapefromuniversity/model/GameModelImp.java
+++ src/main/java/escapefromuniversity/model/GameModelImp.java
@@ -10,23 +10,35 @@ import escapefromuniversity.model.map.MapManagerImpl;
 public class GameModelImp implements GameModel{
 	private final MapManager mapManaget = new MapManagerImpl();
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public List<GameObject> getAllGameObj() {
 		return new LinkedList<GameObject>(this.mapManaget.getRoom().getAllGameObject());
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void updateGame(double deltaTime) {
 		this.mapManaget.getRoom().update(deltaTime);
 		
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public boolean isWin() {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public boolean isLost() {
 		return this.mapManaget.gameOver();
