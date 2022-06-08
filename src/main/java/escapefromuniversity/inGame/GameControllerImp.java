a2e040e27e6948d617d0b9159bf8b32d918e1543
Daniel Capannini <daniel12capannini@gmail.com>
2022-06-08 15:28:32 +0200

    in GameControllerImp create a builder

--- src/main/java/escapefromuniversity/inGame/GameControllerImp.java
+++ src/main/java/escapefromuniversity/inGame/GameControllerImp.java
@@ -1,6 +1,16 @@
 package escapefromuniversity.inGame;
 
+import escapefromuniversity.model.GameModel;
+
 public class GameControllerImp implements GameController{
+	private final GameModel model;
+	private final GameView view;
+	
+	public GameControllerImp(GameModel model, GameView view) {
+		this.model = model;
+		this.view = view;
+	}
+	
 
 	@Override
 	public void GameLoop() {
