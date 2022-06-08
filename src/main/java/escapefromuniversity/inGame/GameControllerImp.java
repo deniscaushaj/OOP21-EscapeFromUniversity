8deba97685e016d914ff4e04b2a302baa91f722e
Daniel Capannini <daniel12capannini@gmail.com>
2022-06-08 16:12:43 +0200

    in GameControllerImp crete new metodes updateModel and getGameObjectID

--- src/main/java/escapefromuniversity/inGame/GameControllerImp.java
+++ src/main/java/escapefromuniversity/inGame/GameControllerImp.java
@@ -1,10 +1,15 @@
 package escapefromuniversity.inGame;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import escapefromuniversity.model.GameModel;
 
 public class GameControllerImp implements GameController{
 	private final GameModel model;
 	private final GameView view;
+	private List<Integer> gameObjID = new LinkedList();
 	
 	public GameControllerImp(GameModel model, GameView view) {
 		this.model = model;
@@ -29,6 +34,17 @@ public class GameControllerImp implements GameController{
 	
 	private boolean continueGame() {
 		return !this.model.isLost() && !this.model.isWin();
+	}
+	
+	private void updateModel(long deltaTime) {
+		this.model.updateGame((double) deltaTime);
+		this.gameObjID = this.getGameObjectID();
+	}
+	
+	private List<Integer> getGameObjectID(){
+		return this.model.getAllGameObj().stream()
+				.map(obj -> obj.getID())
+				.collect(Collectors.toList());
 	}
 
 }
