6a8109b9dd0a14d33db3b69b6a5553d53a661ac1
MarcoAntolini <93138565+MarcoAntolini@users.noreply.github.com>
2022-06-09 04:08:33 +0200

    create draw method to draw components on the game jframe

--- src/main/java/escapefromuniversity/GameView.java
+++ src/main/java/escapefromuniversity/GameView.java
@@ -1,5 +1,6 @@
 package escapefromuniversity;
 
+import java.awt.Graphics2D;
 import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 
@@ -12,6 +13,8 @@ import static escapefromuniversity.model.utilities.LauncherResizer.*;
 public class GameView {
 
     BufferedImage tempScreen;
+    BufferedImage tempScreen = new BufferedImage(screenWidth, screenHeight, BufferedImage.TYPE_INT_ARGB);
+    Graphics2D g2d = (Graphics2D)tempScreen.getGraphics();
     private GameState state;
     
     public static void startGame () {
@@ -39,4 +42,45 @@ public class GameView {
     public void setGameState (GameState gameState) {
         this.state = gameState;
     }
+    
+    public void draw (Graphics2D g2d) {
+        this.g2d = g2d;
+//      g2d.setFont(null); TODO choose font
+        switch (this.getGameState()) {
+        case CHARACTER:
+            this.addPauseBG();
+            break;
+        case DIALOGUE:
+            this.addPauseBG();
+            break;
+        case FIGHT:
+            break;
+        case MENU:
+            this.addPauseBG();
+            break;
+        case PLAY:
+            this.removePauseBG();
+            break;
+        case QUIZ:
+            this.addPauseBG();
+            break;
+        case SHOP:
+            this.addPauseBG();
+            break;
+        case WIN:
+            break;
+        default:
+            break;
+        }
+    }
+    
+    public void addPauseBG () {
+       
+    }
+    
+    public void removePauseBG () {
+        
+    }
+    
+    
 }
