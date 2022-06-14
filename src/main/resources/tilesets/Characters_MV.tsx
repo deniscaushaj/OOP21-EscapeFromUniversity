2363c4c10f908e90da8aac30f83022a877c5fbbc
Denis Caushaj <denis.caushaj@studio.unibo.it>
2022-06-14 16:31:07 +0200

    Create directory tilesets and move tsx files in

--- src/main/resources/tilesets/Characters_MV.tsx
+++ src/main/resources/tilesets/Characters_MV.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.8" tiledversion="1.8.4" name="Characters_MV" tilewidth="48" tileheight="48" tilecount="192" columns="12">
- <image source="../../../Desktop/Characters_MV.png" trans="ffffff" width="576" height="768"/>
+ <image source="Characters_MV.png" trans="ffffff" width="576" height="768"/>
 </tileset>
--- src/main/resources/tilesets/Interiors_free_48x48v2.tsx
+++ src/main/resources/tilesets/Interiors_free_48x48v2.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.8" tiledversion="1.8.4" name="Interiors_free_48x48v2" tilewidth="48" tileheight="48" tilecount="1424" columns="16">
- <image source="../../../Desktop/Interiors_free_48x48v2.png" trans="ffffff" width="768" height="4272"/>
+ <image source="Interiors_free_48x48v2.png" trans="ffffff" width="768" height="4272"/>
 </tileset>
