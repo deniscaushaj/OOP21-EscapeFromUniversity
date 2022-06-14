d8053f39dbc7e996dffbbca8d41b80f97d007f17
Daniel Capannini <daniel12capannini@gmail.com>
2022-06-14 13:45:03 +0200

    create new class GameViewImpl

---
+++ src/main/java/escapefromuniversity/inGame/GameViewImpl.java
@@ -0,0 +1,29 @@
+package escapefromuniversity.inGame;
+
+public class GameViewImpl implements GameView{
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void isLost() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void isWin() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setGameController(GameController controller) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
