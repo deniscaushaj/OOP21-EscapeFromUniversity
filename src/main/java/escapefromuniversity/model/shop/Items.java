8be11fca63b3678e19d2a817b1275992a1d60aa4
Denis Caushaj <denis.caushaj@studio.unibo.it>
2022-06-16 01:22:29 +0200

    Implement Items enum methods

--- src/main/java/escapefromuniversity/model/shop/Items.java
+++ src/main/java/escapefromuniversity/model/shop/Items.java
@@ -1,6 +1,39 @@
 package escapefromuniversity.model.shop;
 
+import escapefromuniversity.model.bullet.Bullet;
+import escapefromuniversity.model.gameObject.GameObject;
+import escapefromuniversity.model.gameObject.GameObjectType;
+import escapefromuniversity.model.player.Player;
+import escapefromuniversity.model.quiz.Anwser;
+import escapefromuniversity.model.quiz.Quiz;
+
 public enum Items {
 
+    INCREASE_ATTACK, INCREASE_DEFENSE, RESET_HEALTH, DOUBLE_CHANCE;
 
+    public void increaseAttack(GameObject gameObject, Bullet bullet){
+        if(gameObject.getType() == GameObjectType.PLAYER){
+            bullet.setDamage(bullet.getDamage() * 150 / 100);
+        }
+    }
+
+    public void increaseDefense(GameObject gameObject, Bullet bullet){
+        if(gameObject.getType() == GameObjectType.BOSS1 || gameObject.getType() == GameObjectType.BOSS2 ||
+                gameObject.getType() == GameObjectType.BOSS3 || gameObject.getType() == GameObjectType.BOSS4 ||
+                gameObject.getType() == GameObjectType.BOSS5 || gameObject.getType() == GameObjectType.BOSS6){
+            bullet.setDamage(bullet.getDamage() * 50 / 100);
+        }
+    }
+
+    public void resetHealth(Player player){
+        if(player.getLife() != player.getMaxLife()){
+            player.setLife(player.getMaxLife());
+        }
+    }
+
+    public void doubleChance(Anwser ans, Quiz quiz){
+        if(!ans.isCorrect()){
+            quiz.doubleChance();
+        }
+    }
 }
