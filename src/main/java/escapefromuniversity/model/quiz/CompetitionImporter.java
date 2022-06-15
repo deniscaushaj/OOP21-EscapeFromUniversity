55d53cd5e042b46f8f14b6e39a302556db6ae54b
Emanuele Bertolero <emanuele.bertolero@studio.unibo.it>
2022-06-15 09:20:42 +0200

    Tests prepared for CompetitionImporter

--- src/main/java/escapefromuniversity/model/quiz/CompetitionImporter.java
+++ src/main/java/escapefromuniversity/model/quiz/CompetitionImporter.java
@@ -13,21 +13,29 @@ public class CompetitionImporter {
 	}
 	
 	public CompetitionImpl importCompetition() {
-		CompetitionImpl competition;
+		
+		CompetitionImpl competition = null;
 		Builder competitionBuilder = new Builder();
 		
 		JSONObject obj = new JSONObject(path);
-		
 		competitionBuilder.setTeacher(obj.getString("teacher"));
 		competitionBuilder.setSubject(obj.getString("subject"));
 
 		JSONArray arr = obj.getJSONArray("quiz");
 		
+		//Cicle for quiz
 		for (int i = 0; i < arr.length(); i++)
 		{
-			
-		    String post_id = arr.getJSONObject(i).getString("post_id");
+		    String question = arr.getJSONObject(i).getString("question");
+		    JSONArray answer = arr.getJSONObject(i).getJSONArray("answer");
+		    String a = answer.getJSONObject(0).getString("a");
+		    String b = answer.getJSONObject(1).getString("b");
+		    String c = answer.getJSONObject(2).getString("c");
+		    String d = answer.getJSONObject(3).getString("d");
+		    String correct = arr.getJSONObject(i).getString("correct");
 		}
+		
+		return competition;
 	}
 	
 	
