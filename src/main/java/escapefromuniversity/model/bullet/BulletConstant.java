64d15be982d3f3a1b035319d8b536e7c3af4c45e
Daniel Capannini <daniel12capannini@gmail.com>
2022-06-16 15:11:34 +0200

    in BulletConstant modify value

--- src/main/java/escapefromuniversity/model/bullet/BulletConstant.java
+++ src/main/java/escapefromuniversity/model/bullet/BulletConstant.java
@@ -4,29 +4,29 @@ import escapefromuniversity.model.basics.Point2D;
 
 public class BulletConstant {
 	
-	protected final static Point2D BULLET_BOX_SIZE = new Point2D(3,1);
+	protected final static Point2D BULLET_BOX_SIZE = new Point2D(5,5);
 	
 	/**
 	 * Constant of bullets speed.
 	 */
-	protected static final int PLAYER_BULLET_SPEED = 0;
-	protected static final int BOSS_1_BULLET_SPEED = 0;
-	protected static final int BOSS_2_BULLET_SPEED = 0;
-	protected static final int BOSS_3_BULLET_SPEED = 0;
-	protected static final int BOSS_4_BULLET_SPEED = 0;
-	protected static final int BOSS_5_BULLET_SPEED = 0;
-	protected static final int BOSS_6_BULLET_SPEED = 0;
+	protected static final int PLAYER_BULLET_SPEED = 30;
+	protected static final int BOSS_1_BULLET_SPEED = 30;
+	protected static final int BOSS_2_BULLET_SPEED = 25;
+	protected static final int BOSS_3_BULLET_SPEED = 25;
+	protected static final int BOSS_4_BULLET_SPEED = 30;
+	protected static final int BOSS_5_BULLET_SPEED = 35;
+	protected static final int BOSS_6_BULLET_SPEED = 40;
 	
 	
 	/**
 	 * Constants of bullets damage.
 	 */
-	protected static final int PLAYER_BULLET_DAMAGE = 0;
-	protected static final int BOSS_1_BULLET_DAMAGE = 0;
-	protected static final int BOSS_2_BULLET_DAMAGE = 0;
-	protected static final int BOSS_3_BULLET_DAMAGE = 0;
-	protected static final int BOSS_4_BULLET_DAMAGE = 0;
-	protected static final int BOSS_5_BULLET_DAMAGE = 0;
-	protected static final int BOSS_6_BULLET_DAMAGE = 0;
+	protected static final int PLAYER_BULLET_DAMAGE = 15;
+	protected static final int BOSS_1_BULLET_DAMAGE = 15;
+	protected static final int BOSS_2_BULLET_DAMAGE = 20;
+	protected static final int BOSS_3_BULLET_DAMAGE = 30;
+	protected static final int BOSS_4_BULLET_DAMAGE = 35;
+	protected static final int BOSS_5_BULLET_DAMAGE = 25;
+	protected static final int BOSS_6_BULLET_DAMAGE = 10;
 
 }
