9911eaf58cb64668fc78749d302c9ca4c9727eed
MarcoAntolini <marco.antolini2001@gmail.com>
2022-06-16 16:22:46 +0200

    fix method indentation and remove final from method

--- src/main/java/escapefromuniversity/launcher/LauncherView.java
+++ src/main/java/escapefromuniversity/launcher/LauncherView.java
@@ -18,15 +18,15 @@ public final class LauncherView extends Application{
 
     static Stage launcherWindow;
     static Scene launcher;
-	
-    public final void start(Stage stage) throws Exception {
+
+    public void start(Stage stage) throws Exception {
         FXMLLoader loader = new FXMLLoader();
-	URL fileLocation = new File(OSFixes.getLocation("layouts","Launcher.fxml")).toURI().toURL();
-	loader.setLocation(fileLocation);
-    	Parent startRoot = loader.load();
-    	LauncherResizer launcherResizer = new LauncherResizer();
-    	launcherResizer.setSceneSize(screenWidth, screenHeight);
-    	launcher = new Scene(startRoot, sceneWidth, sceneHeight);
+        URL fileLocation = new File(OSFixes.getLocation("layouts","Launcher.fxml")).toURI().toURL();
+        loader.setLocation(fileLocation);
+        Parent startRoot = loader.load();
+        LauncherResizer launcherResizer = new LauncherResizer();
+        launcherResizer.setSceneSize(screenWidth, screenHeight);
+        launcher = new Scene(startRoot, sceneWidth, sceneHeight);
         stage.setTitle("Escape From University");
         stage.setScene(launcher);
         stage.setResizable(false);
@@ -36,9 +36,9 @@ public final class LauncherView extends Application{
         stage.show();
         launcherWindow = stage;
     }
-	
+
     public static void createLauncher(final String[] args) {
         launch();
     }
-	
+
 }
