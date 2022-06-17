dcacb9911c66ffed9b0092c1e8885fa0154adc4d
Daniel Capannini <daniel12capannini@gmail.com>
2022-06-17 13:17:54 +0200

    in Point2D generet hashCode and equals

--- src/main/java/escapefromuniversity/model/basics/Point2D.java
+++ src/main/java/escapefromuniversity/model/basics/Point2D.java
@@ -1,5 +1,7 @@
 package escapefromuniversity.model.basics;
 
+import java.util.Objects;
+
 public class Point2D {
 	
 	private final double x;
@@ -10,7 +12,25 @@ public class Point2D {
 		 this.y = y;
 	 }
 	 
-	 public Point2D(Point2D point) {
+	 @Override
+	public int hashCode() {
+		return Objects.hash(x, y);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Point2D other = (Point2D) obj;
+		return Double.doubleToLongBits(x) == Double.doubleToLongBits(other.x)
+				&& Double.doubleToLongBits(y) == Double.doubleToLongBits(other.y);
+	}
+
+	public Point2D(Point2D point) {
 		 this.x = point.getX();
 		 this.y = point.getY();
 	 }
