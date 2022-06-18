834fd3f55e4b2273fb1fed7544998f039542a5bc
Denis Caushaj <denis.caushaj@studio.unibo.it>
2022-06-18 20:07:01 +0200

    Add JavaDoc

--- src/main/java/escapefromuniversity/model/map/Obstacle.java
+++ src/main/java/escapefromuniversity/model/map/Obstacle.java
@@ -2,5 +2,8 @@ package escapefromuniversity.model.map;
 
 import escapefromuniversity.model.gameObject.StaticGameObject;
 
+/**
+ * A interface which models an obstacle
+ */
 public interface Obstacle extends StaticGameObject {
 }
