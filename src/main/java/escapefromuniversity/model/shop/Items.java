bd3da9277245d85264a6f2d83891368f2201513f
Denis Caushaj <denis.caushaj@studio.unibo.it>
2022-06-18 17:22:14 +0200

    Replace parameters with a field in Items enum

--- src/main/java/escapefromuniversity/model/shop/Items.java
+++ src/main/java/escapefromuniversity/model/shop/Items.java
@@ -10,13 +10,13 @@ public enum Items {
 
     private static final int ATT_BUFF = 10;
     private static final int DEF_BUFF = 10;
+    private Player player;
 
     /**
      * Increase player's attack
-     * @param player the current player
      * @return true if current damage is less then max damage, false otherwise
      */
-    public boolean increaseAttack(Player player){
+    public boolean increaseAttack(){
         var maxDam = player.getMaxDamage();
         var currDam = player.getDamage();
         if(currDam < maxDam){
@@ -28,10 +28,9 @@ public enum Items {
 
     /**
      * Increase player's armor
-     * @param player the current player
      * @return true if current armor is less then max armor, false otherwise
      */
-    public boolean increaseDefense(Player player){
+    public boolean increaseDefense(){
         var maxDef = player.getMaxArmor();
         var currDef = player.getArmor();
         if(currDef < maxDef){
@@ -43,9 +42,8 @@ public enum Items {
 
     /**
      * Reset player's health
-     * @param player the current player
      */
-    public void resetHealth(Player player){
+    public void resetHealth(){
         var maxLife = player.getMaxLife();
         var currLife = player.getLife();
         if(currLife < maxLife){
