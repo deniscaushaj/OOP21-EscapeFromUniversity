e4bb405008c74db6296e511e2324fd8efffa0439
Daniel Capannini <daniel12capannini@gmail.com>
2022-06-18 10:48:03 +0200

    in GameController added comment

--- src/main/java/escapefromuniversity/inGame/GameController.java
+++ src/main/java/escapefromuniversity/inGame/GameController.java
@@ -6,20 +6,49 @@ import java.awt.event.KeyEvent;
 
 public interface GameController {
 	
-	
+	/**
+	 * main game loop.
+	 */
 	void gameLoop();
 	
+	/**
+	 * 
+	 * @return life of player
+	 */
 	int getPlayerLife();
 	
+	/**
+	 * 
+	 * @return credits of player
+	 */
 	int getPlayerCredits();
 	
+	/**
+	 * play the quiz
+	 */
 	void goQuiz();
 
+	/**
+	 * 
+	 * @return current gameState.
+	 */
 	GameState getGameState();
 
+	/**
+	 * 
+	 * @param gameState: new gameState.
+	 */
 	void setGameState(GameState gameState);
 
+	/**
+	 * 
+	 * @param key
+	 */
 	void pressKey(KeyEvent key);
 
+	/**
+	 * 
+	 * @param key
+	 */
 	void releaseKey(KeyEvent key);
 }
