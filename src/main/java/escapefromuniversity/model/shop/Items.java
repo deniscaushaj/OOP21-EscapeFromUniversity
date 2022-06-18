feb5e9c281458a4618640cf50abbbc5eaa1ad837
Denis Caushaj <denis.caushaj@studio.unibo.it>
2022-06-18 11:42:13 +0200

    Change methods increaseDefense and increaseAttack from void to boolean, add constant fields and add doc in Items enum

--- src/main/java/escapefromuniversity/model/shop/Items.java
+++ src/main/java/escapefromuniversity/model/shop/Items.java
@@ -8,23 +8,59 @@ public enum Items {
 
     INCREASE_ATTACK, INCREASE_DEFENSE, RESET_HEALTH, DOUBLE_CHANCE;
 
-    public void increaseDamage(Player player, int damageBuff){
-//        if(player.getDamage() < player.getMaxDamage()){} TODO l'if è da usare fuori quando chiami il metodo, altrimenti così puoi comprarlo sempre e poi l'if a volte lo rende inefficace
-        player.setDamageBuff(damageBuff);
+    private static final int ATT_BUFF = 10;
+    private static final int DEF_BUFF = 10;
+
+    /**
+     * Increase player's attack
+     * @param player the current player
+     * @return true if current damage is less then max damage, false otherwise
+     */
+    public boolean increaseAttack(Player player){
+        var maxDam = player.getMaxDamage();
+        var currDam = player.getDamage();
+        if(currDam < maxDam){
+            player.setDamageBuff(currDam+ATT_BUFF);
+            return true;
+        }
+        return false;
     }
 
-    public void increaseArmor(Player player, int armorBuff){
-//        if(player.getArmor() < player.getMaxArmor()){} TODO idem
-        player.setArmorBuff(armorBuff);
+    /**
+     * Increase player's armor
+     * @param player the current player
+     * @return true if current armor is less then max armor, false otherwise
+     */
+    public boolean increaseDefense(Player player){
+        var maxDef = player.getMaxArmor();
+        var currDef = player.getArmor();
+        if(currDef < maxDef){
+            player.setArmorBuff(currDef+DEF_BUFF);
+            return true;
+        }
+        return false;
     }
 
+    /**
+     * Reset player's health
+     * @param player the current player
+     */
     public void resetHealth(Player player){
-//        if(player.getLife() != player.getMaxLife()){} TODO idem
-        player.resetLife();
+        var maxLife = player.getMaxLife();
+        var currLife = player.getLife();
+        if(currLife < maxLife){
+            player.resetLife();
+        }
     }
 
+    /**
+     * Gives another chance to the player when the answer is incorrect
+     * @param ans the answer given by the user
+     * @param competition the current exam
+     */
     public void doubleChance(Answer ans, Competition competition){
-//        if(!ans.isCorrect() && !competition.getBonusQuiz()){} TODO idem
-        competition.setBonusQuiz(true);
+        if(!ans.isCorrect() && !competition.getBonusQuiz()){
+            competition.setBonusQuiz(true);
+        }
     }
 }
