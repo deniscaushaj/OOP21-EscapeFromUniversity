103783f5f62a1c96f9e516fb125320e979e6d795
Daniel Capannini <daniel12capannini@gmail.com>
2022-06-20 19:29:53 +0200

    in GameHUDPanel override metode paintComponet

--- src/main/java/escapefromuniversity/inGame/GameHUDPanel.java
+++ src/main/java/escapefromuniversity/inGame/GameHUDPanel.java
@@ -19,13 +19,15 @@ public class GameHUDPanel extends JLayeredPane{
 	private final Font font = new Font("OCR A Extended", Font.PLAIN, 20);
 	private final Image creditsImage;
 	private final Rectangle playerLifeBarPosition;
+	private final Rectangle creditsPosition;
+	private final Rectangle creditsCounterPosition;
 	
 	GameHUDPanel(final double windowRatio){
 		this.creditsCounter.setText("0");
 		this.creditsCounter.setFont(font);
 		this.creditsCounter.setForeground(Color.white);
-		Rectangle creditsPosition = new Rectangle((int) (10 * windowRatio), (int) (50 * windowRatio), (int) (50 * windowRatio), (int) (50 * windowRatio));
-		Rectangle creditsCounterPosition = new Rectangle((int) (60 * windowRatio), (int) (50 * windowRatio), (int) (50 * windowRatio), (int) (50 * windowRatio));
+		this.creditsPosition = new Rectangle((int) (10 * windowRatio), (int) (50 * windowRatio), (int) (50 * windowRatio), (int) (50 * windowRatio));
+		this.creditsCounterPosition = new Rectangle((int) (60 * windowRatio), (int) (50 * windowRatio), (int) (50 * windowRatio), (int) (50 * windowRatio));
 		this.creditsCounter.setBounds(creditsCounterPosition);
 		this.creditsImage = new ImageIcon(this.getClass().getResource("file immagine")).getImage().getScaledInstance((int) creditsPosition.getWidth(),
                 (int) creditsPosition.getHeight(), 
@@ -49,6 +51,13 @@ public class GameHUDPanel extends JLayeredPane{
 	
 	public void updateLife(final int life) {
 		this.playerLifeBar.setValue(life);
+	}
+	
+	@Override
+	public void paintComponent(final Graphics g) {
+		super.paintComponent(g);
+		g.drawImage(this.creditsImage, this.creditsPosition.getX(), this.creditsPosition.getY(), null);
+		Toolkit.getDefaultToolkit().sync();
 	}
 
 }
