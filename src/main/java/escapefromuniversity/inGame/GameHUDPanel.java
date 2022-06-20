d04aed850cacee8287326e31879a338111c5700c
Daniel Capannini <daniel12capannini@gmail.com>
2022-06-20 19:16:56 +0200

    in GameHUDPanel create new metode setLifeBar

--- src/main/java/escapefromuniversity/inGame/GameHUDPanel.java
+++ src/main/java/escapefromuniversity/inGame/GameHUDPanel.java
@@ -4,13 +4,16 @@ import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JLayeredPane;
 import javax.swing.JProgressBar;
-import javax.swing.border.Border;
 
 import java.awt.Rectangle;
 import java.awt.*;
 
 public class GameHUDPanel extends JLayeredPane{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private final JLabel creditsCounter = new JLabel();
 	private JProgressBar playerLifeBar;
 	private final Font font = new Font("OCR A Extended", Font.PLAIN, 20);
@@ -28,9 +31,17 @@ public class GameHUDPanel extends JLayeredPane{
                 (int) creditsPosition.getHeight(), 
                 Image.SCALE_SMOOTH);
 		this.playerLifeBarPosition = new Rectangle((int) (10 * windowRatio), (int) (10 * windowRatio), (int) (300 * windowRatio), (int) (30 * windowRatio));
+		this.add(this.creditsCounter);
+		this.setOpaque(false);
 	}
 	
-	
+	public void setLifeBar(final int maxLife) {
+		this.remove(this.playerLifeBar);
+		this.playerLifeBar = new JProgressBar(0, maxLife);
+		this.playerLifeBar.setBounds(this.playerLifeBarPosition);
+		this.playerLifeBar.setForeground(new Color(150, 0, 0));
+		this.add(this.playerLifeBar);
+	}
 	
 	public void updateCreditsCounter(final int credits) {
 		this.creditsCounter.setText("" + credits);
