5907382bf5872f848c6b6b839184b17dc9b1032e
Daniel Capannini <daniel12capannini@gmail.com>
2022-06-20 16:25:25 +0200

    create class WindowSet

---
+++ src/main/java/escapefromuniversity/inGame/WindowSet.java
@@ -0,0 +1,20 @@
+package escapefromuniversity.inGame;
+
+import static escapefromuniversity.utilities.LauncherResizer.screenHeight;
+import static escapefromuniversity.utilities.LauncherResizer.screenWidth;
+
+public class WindowSet {
+	
+	public static final int NATIVE_WIDTH = 1920;
+	
+	public static final int NATIVE_HEIGHT = 1080;
+	
+	public static final double RATIO = 0.777;
+	
+	private static final double windowRatio = Math.min(screenHeight / NATIVE_HEIGHT, screenWidth / NATIVE_WIDTH);
+	
+	public double getWindowRatio() {
+		return windowRatio;
+	}
+
+}
