7fdc5ebf549575815c2a240c914854e75aeb8c01
Daniel Capannini <daniel12capannini@gmail.com>
2022-06-22 00:13:12 +0200

    correction for chekstyle in BulletFactoryImpl

--- src/main/java/escapefromuniversity/model/bullet/BulletFactoryImpl.java
+++ src/main/java/escapefromuniversity/model/bullet/BulletFactoryImpl.java
@@ -5,69 +5,74 @@ import escapefromuniversity.model.basics.Vector2D;
 import escapefromuniversity.model.gameObject.GameObjectType;
 import escapefromuniversity.model.map.Room;
 
-public class BulletFactoryImpl implements BulletFactory{
-	
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public Bullet createPlayerBullet(Point2D startPosition, Vector2D direction, int damage, Room room) {
-		return new BulletImpl(GameObjectType.BULLET_PLAYER, startPosition,BulletConstant.PLAYER_BULLET_SPEED,
-				             direction, damage, room);
-	}
+/**
+ * 
+ * class BulletFactoryImpl implement interface BulletFactory.
+ *
+ */
+public class BulletFactoryImpl implements BulletFactory {
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public Bullet createBoss1Bullet(Point2D startPosition, Vector2D direction, Room room) {
-		return new BulletImpl(GameObjectType.BULLET_BOSS_1, startPosition,BulletConstant.BOSS_1_BULLET_SPEED,
-	                         direction, BulletConstant.BOSS_1_BULLET_DAMAGE, room);
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Bullet createPlayerBullet(final Point2D startPosition, final Vector2D direction, final int damage, final Room room) {
+        return new BulletImpl(GameObjectType.BULLET_PLAYER, startPosition, BulletConstant.PLAYER_BULLET_SPEED,
+                              direction, damage, room);
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public Bullet createBoss2Bullet(Point2D startPosition, Vector2D direction, Room room) {
-		return new BulletImpl(GameObjectType.BULLET_BOSS_2, startPosition,BulletConstant.BOSS_2_BULLET_SPEED,
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Bullet createBoss1Bullet(final Point2D startPosition, final Vector2D direction, final Room room) {
+        return new BulletImpl(GameObjectType.BULLET_BOSS_1, startPosition, BulletConstant.BOSS_1_BULLET_SPEED,
+                             direction, BulletConstant.BOSS_1_BULLET_DAMAGE, room);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Bullet createBoss2Bullet(final Point2D startPosition, final Vector2D direction, final Room room) {
+        return new BulletImpl(GameObjectType.BULLET_BOSS_2, startPosition, BulletConstant.BOSS_2_BULLET_SPEED,
                              direction, BulletConstant.BOSS_2_BULLET_DAMAGE, room);
-	}
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public Bullet createBoss3Bullet(Point2D startPosition, Vector2D direction, Room room) {
-		return new BulletImpl(GameObjectType.BULLET_BOSS_3, startPosition,BulletConstant.BOSS_3_BULLET_SPEED,
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Bullet createBoss3Bullet(final Point2D startPosition, final Vector2D direction, final Room room) {
+        return new BulletImpl(GameObjectType.BULLET_BOSS_3, startPosition, BulletConstant.BOSS_3_BULLET_SPEED,
                              direction, BulletConstant.BOSS_3_BULLET_DAMAGE, room);
-	}
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public Bullet createBoss4Bullet(Point2D startPosition, Vector2D direction, Room room) {
-		return new BulletImpl(GameObjectType.BULLET_BOSS_4, startPosition,BulletConstant.BOSS_4_BULLET_SPEED,
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Bullet createBoss4Bullet(final Point2D startPosition, final Vector2D direction, final Room room) {
+        return new BulletImpl(GameObjectType.BULLET_BOSS_4, startPosition, BulletConstant.BOSS_4_BULLET_SPEED,
                              direction, BulletConstant.BOSS_4_BULLET_DAMAGE, room);
-	}
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public Bullet createBoss5Bullet(Point2D startPosition, Vector2D direction, Room room) {
-		return new BulletImpl(GameObjectType.BULLET_BOSS_5, startPosition,BulletConstant.BOSS_5_BULLET_SPEED,
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Bullet createBoss5Bullet(final Point2D startPosition, final Vector2D direction, final Room room) {
+        return new BulletImpl(GameObjectType.BULLET_BOSS_5, startPosition, BulletConstant.BOSS_5_BULLET_SPEED,
                              direction, BulletConstant.BOSS_5_BULLET_DAMAGE, room);
-	}
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public Bullet createBoss6Bullet(Point2D startPosition, Vector2D direction, Room room) {
-		return new BulletImpl(GameObjectType.BULLET_BOSS_6, startPosition,BulletConstant.BOSS_6_BULLET_SPEED,
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Bullet createBoss6Bullet(final Point2D startPosition, final Vector2D direction, final Room room) {
+        return new BulletImpl(GameObjectType.BULLET_BOSS_6, startPosition, BulletConstant.BOSS_6_BULLET_SPEED,
                              direction, BulletConstant.BOSS_6_BULLET_DAMAGE, room);
-	}
+    }
 
 }
