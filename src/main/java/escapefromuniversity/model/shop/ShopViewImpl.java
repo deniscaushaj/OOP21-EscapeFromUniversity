c1fa6d11ce5799f2ba211964a17a7d5d8db3e45a
MarcoAntolini <marco.antolini2001@gmail.com>
2022-06-22 00:23:18 +0200

    remove methods and initialize some fields

--- src/main/java/escapefromuniversity/model/shop/ShopViewImpl.java
+++ src/main/java/escapefromuniversity/model/shop/ShopViewImpl.java
@@ -9,24 +9,27 @@ import java.awt.*;
 public class ShopViewImpl extends JPanel implements ShopView {
 
     private final ShopController shopController;
-
-    private Items itemType;
     private final double windowRatio = WindowSet.getWindowRatio();
     private final JLabel creditsCounter = new JLabel("Credits", (Icon) this.creditsIcon, SwingConstants.CENTER);
     private JTextArea itemInfo;
-    private JButton buyLife, buyArmor, buyDamage, buyChance, exit;
+    private final JButton buyLife = new JButton("", this.creditsIcon);
+    private final JButton buyArmor = new JButton("", this.creditsIcon);
+    private final JButton buyDamage = new JButton("", this.creditsIcon);
+    private final JButton buyChance = new JButton("", this.creditsIcon);
+    private final JButton exit = new JButton("", this.creditsIcon);
     private final ImageIcon creditsIcon = new ImageIcon(OSFixes.getLocation("hud", "credits.png"));
     private final ImageIcon lifeIcon = new ImageIcon(OSFixes.getLocation("hud", "life.png"));
     private final ImageIcon armorIcon = new ImageIcon(OSFixes.getLocation("hud", "armor.png"));
     private final ImageIcon damageIcon = new ImageIcon(OSFixes.getLocation("hud", "damage.png"));
     private final ImageIcon chanceIcon = new ImageIcon(OSFixes.getLocation("hud", "second_chance.png"));
     private final Rectangle creditsCounterPos = new Rectangle((int) (60 * windowRatio), (int) (50 * windowRatio), (int) (50 * windowRatio), (int) (50 * windowRatio));
-    private final Rectangle itemInfoPos = new Rectangle((int) (60 * windowRatio), (int) (50 * windowRatio), (int) (50 * windowRatio), (int) (50 * windowRatio));
+    private final Rectangle itemInfoPos = new Rectangle((int) (10 * windowRatio), (int) (10 * windowRatio), (int) (50 * windowRatio), (int) (50 * windowRatio));
     private final Rectangle buyLifePos = new Rectangle((int) (60 * windowRatio), (int) (50 * windowRatio), (int) (50 * windowRatio), (int) (50 * windowRatio));
     private final Rectangle buyArmorPos = new Rectangle((int) (60 * windowRatio), (int) (50 * windowRatio), (int) (50 * windowRatio), (int) (50 * windowRatio));
     private final Rectangle buyDamagePos = new Rectangle((int) (60 * windowRatio), (int) (50 * windowRatio), (int) (50 * windowRatio), (int) (50 * windowRatio));
     private final Rectangle buyChancePos = new Rectangle((int) (60 * windowRatio), (int) (50 * windowRatio), (int) (50 * windowRatio), (int) (50 * windowRatio));
     private final Rectangle exitPos = new Rectangle((int) (60 * windowRatio), (int) (50 * windowRatio), (int) (50 * windowRatio), (int) (50 * windowRatio));
+//    private final Rectangle iconspos
 
     public ShopViewImpl(final ShopController shopController) { // TODO chiamare con WindowSet.getWindowRatio()t
         this.shopController = shopController;
@@ -45,19 +48,19 @@ public class ShopViewImpl extends JPanel implements ShopView {
 
         this.buyLife.addActionListener(e -> {
             this.shopController.buyItem();
-            this.setItemType(Items.RESET_HEALTH);
+            this.shopController.setItemType(Items.RESET_HEALTH);
         });
         this.buyArmor.addActionListener(e -> {
             this.shopController.buyItem();
-            this.setItemType(Items.INCREASE_ARMOR);
+            this.shopController.setItemType(Items.INCREASE_ARMOR);
         });
         this.buyDamage.addActionListener(e -> {
             this.shopController.buyItem();
-            this.setItemType(Items.INCREASE_DAMAGE);
+            this.shopController.setItemType(Items.INCREASE_DAMAGE);
         });
         this.buyChance.addActionListener(e -> {
             this.shopController.buyItem();
-            this.setItemType(Items.DOUBLE_CHANCE);
+            this.shopController.setItemType(Items.DOUBLE_CHANCE);
         });
         this.exit.addActionListener(e -> this.shopController.closeShop());
 
@@ -70,17 +73,6 @@ public class ShopViewImpl extends JPanel implements ShopView {
         this.add(this.buyChance);
         this.add(this.exit);
         this.setVisible(true);
-    }
-
-    // TODO non vanno qua:
-    @Override
-    public void setItemType(Items itemType) {
-        this.itemType = itemType;
-    }
-
-    @Override
-    public Items getItemType() {
-        return this.itemType;
     }
 
 }
