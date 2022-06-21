2ba48133c37c1e44b4d43abe3255a049f7b565b2
Daniel Capannini <daniel12capannini@gmail.com>
2022-06-22 00:57:11 +0200

    correction for chekstyle in Boss

--- src/main/java/escapefromuniversity/model/enemy/Boss.java
+++ src/main/java/escapefromuniversity/model/enemy/Boss.java
@@ -1,39 +1,55 @@
 package escapefromuniversity.model.enemy;
 
-public interface Boss extends Enemy{
-	
-	enum BossState{
-		QUIZ,
-		FIGHT;
-	}
-	
-	/**
-	 * 
-	 * @return damage of collision with boss.
-	 */
-	int getImpactDamage();
-	
-	/**
-	 * 
-	 * @return state of Boss.
-	 */
-	BossState getBossState();
-	
-	/**
-	 * 
-	 * @param state: new state.
-	 */
-	void setBossState(BossState state);
-	
-	/**
-	 * 
-	 * @param result: point in quiz.
-	 */
-	void setQuizResult(int result);
-	
-	/**
-	 * delete this object.
-	 */
-	void kill();
+/**
+ * 
+ * interface Boss.
+ *
+ */
+public interface Boss extends Enemy {
+
+    /**
+     * 
+     * enum BossState.
+     *
+     */
+    enum BossState {
+        /**
+         * 
+         */
+        QUIZ,
+        /**
+         * 
+         */
+        FIGHT;
+    }
+
+    /**
+     * 
+     * @return damage of collision with boss.
+     */
+    int getImpactDamage();
+
+    /**
+     * 
+     * @return state of Boss.
+     */
+    BossState getBossState();
+
+    /**
+     * 
+     * @param state : new state.
+     */
+    void setBossState(BossState state);
+
+    /**
+     * 
+     * @param result : point in quiz.
+     */
+    void setQuizResult(int result);
+
+    /**
+     * delete this object.
+     */
+    void kill();
 
 }
