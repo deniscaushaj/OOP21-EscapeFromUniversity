05cc2b44936976943f6d283d9a055544b3e1079a
MarcoAntolini <marco.antolini2001@gmail.com>
2022-06-21 17:28:40 +0200

    refactor class

--- src/main/java/escapefromuniversity/utilities/WindowSet.java
+++ src/main/java/escapefromuniversity/utilities/WindowSet.java
@@ -1,19 +1,19 @@
-package escapefromuniversity.inGame;
+package escapefromuniversity.utilities;
+
+import java.awt.*;
 
 import static escapefromuniversity.utilities.LauncherResizer.screenHeight;
 import static escapefromuniversity.utilities.LauncherResizer.screenWidth;
 
 public class WindowSet {
-	
+
+	public static final Font FONT = new Font("OCR A Extended", Font.PLAIN, 20);
 	public static final int NATIVE_WIDTH = 1920;
-	
 	public static final int NATIVE_HEIGHT = 1080;
-	
 	public static final double RATIO = 0.777;
-	
-	private static final double windowRatio = Math.min(screenHeight / NATIVE_HEIGHT, screenWidth / NATIVE_WIDTH);
-	
-	public double getWindowRatio() {
+	private static final double windowRatio = Math.min((float) screenHeight / (float) NATIVE_HEIGHT, (float) screenWidth / (float) NATIVE_WIDTH);
+
+	public static double getWindowRatio() {
 		return windowRatio;
 	}
 
