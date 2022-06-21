978e56d14717b351875cd0d3d65362469fa2ad2b
Daniel Capannini <daniel12capannini@gmail.com>
2022-06-21 11:50:38 +0200

    create class MenuControllerImpl implements interface MenuController

---
+++ src/main/java/escapefromuniversity/menu/MenuControllerImpl.java
@@ -0,0 +1,23 @@
+package escapefromuniversity.menu;
+
+public class MenuControllerImpl implements MenuController{
+
+	@Override
+	public void getCommand() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void exit() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
