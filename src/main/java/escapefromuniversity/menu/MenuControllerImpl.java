3ff4aea6010a4d4ccdca4ad5a32b4b1391248044
Daniel Capannini <daniel12capannini@gmail.com>
2022-06-23 01:00:48 +0200

    correction checkstyle in MenuControllerImpl

--- src/main/java/escapefromuniversity/menu/MenuControllerImpl.java
+++ src/main/java/escapefromuniversity/menu/MenuControllerImpl.java
@@ -3,38 +3,44 @@ package escapefromuniversity.menu;
 import escapefromuniversity.inGame.GameController;
 import escapefromuniversity.model.GameState;
 
-public class MenuControllerImpl implements MenuController{
-	private final GameController gameController;
-	private final MenuView view;
-	
-	public MenuControllerImpl(final GameController gameController) {
-		this.gameController = gameController;
-		this.view = new MenuViewImpl(this);
-	}
+/**
+ * 
+ * implementation interface MenuController.
+ *
+ */
+public class MenuControllerImpl implements MenuController {
+    private final GameController gameController;
+    private final MenuView view;
 
-	@Override
-	public void getCommand() {
-		this.view.startCommandMenu();
-		
-	}
+    /**
+     * 
+     * @param gameController
+     */
+    public MenuControllerImpl(final GameController gameController) {
+        this.gameController = gameController;
+        this.view = new MenuViewImpl(this);
+    }
 
-	@Override
-	public void exit() {
-		System.exit(0);
-		
-	}
+    @Override
+    public void getCommand() {
+        this.view.startCommandMenu();
+    }
 
-	@Override
-	public void resume() {
-		this.view.close();
-		this.gameController.setGameState(GameState.PLAY);
-		
-	}
+    @Override
+    public void exit() {
+        System.exit(0);
+    }
 
-	@Override
-	public void back() {
-		this.view.startMenu();
-	}
+    @Override
+    public void resume() {
+        this.view.close();
+        this.gameController.setGameState(GameState.PLAY);
+    }
+
+    @Override
+    public void back() {
+        this.view.startMenu();
+    }
 
     @Override
     public void startView() {
