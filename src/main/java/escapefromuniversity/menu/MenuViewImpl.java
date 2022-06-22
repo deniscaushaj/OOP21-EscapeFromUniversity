9e49300fe2f6b57f2b2e1347393e91e0d049af87
Daniel Capannini <daniel12capannini@gmail.com>
2022-06-22 15:15:16 +0200

    in MenuViewImpl set font

--- src/main/java/escapefromuniversity/menu/MenuViewImpl.java
+++ src/main/java/escapefromuniversity/menu/MenuViewImpl.java
@@ -1,6 +1,10 @@
 package escapefromuniversity.menu;
 
 import java.awt.Rectangle;
+import java.awt.Image;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLayeredPane;
@@ -10,6 +14,11 @@ import escapefromuniversity.utilities.WindowSet;
 import static escapefromuniversity.utilities.LauncherResizer.screenHeight;
 import static escapefromuniversity.utilities.LauncherResizer.screenWidth;
 
+/**
+ * 
+ * menu in game view.
+ *
+ */
 public class MenuViewImpl implements MenuView {
 	
 	private final JFrame window = new JFrame();
@@ -53,6 +62,9 @@ public class MenuViewImpl implements MenuView {
 		this.btExit.setBounds(this.retBtExit);
 		this.btResume.setBounds(this.retBtResume);
 		this.btCommand.setBounds(this.retBtCommand);
+		this.btExit.setFont(WindowSet.FONT);
+		this.btResume.setFont(WindowSet.FONT);
+		this.btCommand.setFont(WindowSet.FONT);
 		this.btExit.addActionListener(e -> this.controller.exit());
 		this.btResume.addActionListener(e -> this.controller.resume());
 		this.btCommand.addActionListener(e -> this.controller.getCommand());
@@ -66,11 +78,13 @@ public class MenuViewImpl implements MenuView {
 		this.window.setVisible(false);
 	}
 
+	@Override
 	public void startCommandMenu() {
 		this.menuPanel.removeAll();
 		this.menuPanel.revalidate();
 		this.menuPanel.repaint();
 		this.btBack.setBounds(this.retBtBack);
+		this.btBack.setFont(WindowSet.FONT);
 		this.btBack.addActionListener(e -> this.controller.back());
 		this.menuPanel.add(this.btBack);
 	}
