0c5e88a2d091ae2d255c8585905e01f8a0cb9a2a
Daniel Capannini <daniel12capannini@gmail.com>
2022-06-22 08:51:02 +0200

    correction for chekstyle in BossFactory

--- src/main/java/escapefromuniversity/model/enemy/BossConstant.java
+++ src/main/java/escapefromuniversity/model/enemy/BossConstant.java
@@ -13,45 +13,45 @@ final class BossConstant {
     }
 
     protected static final Point2D BOSS_BOX_SIZE = new Point2D(50, 70);
-	
-	/**
-	 * constant boss life.
-	 */
-	protected static final int BOSS_1_LIFE = 200;
-	protected static final int BOSS_2_LIFE = 150;
-	protected static final int BOSS_3_LIFE = 190;
-	protected static final int BOSS_4_LIFE = 100;
-	protected static final int BOSS_5_LIFE = 180;
-	protected static final int BOSS_6_LIFE = 300;
-	
-	/**
-	 * constant boss impact damage.
-	 */
-	protected static final int BOSS_1_DAMAGE = 15;
-	protected static final int BOSS_2_DAMAGE = 10;
-	protected static final int BOSS_3_DAMAGE = 0;
-	protected static final int BOSS_4_DAMAGE = 0;
-	protected static final int BOSS_5_DAMAGE = 5;
-	protected static final int BOSS_6_DAMAGE = 5;
-	
-	/**
-	 * constant boss shoot delay in milliseconds.
-	 */
-	protected static final long BOSS_1_SHOOT_DELAY = 50;
-	protected static final long BOSS_2_SHOOT_DELAY = 250;
-	protected static final long BOSS_3_SHOOT_DELAY = 300;
-	protected static final long BOSS_4_SHOOT_DELAY = 200;
-	protected static final long BOSS_5_SHOOT_DELAY = 250;
-	protected static final long BOSS_6_SHOOT_DELAY = 150;
-	
-	/**
-	 * constant boss speed.
-	 */
-	protected static final int BOSS_1_SPEED = 20;
-	protected static final int BOSS_2_SPEED = 25;
-	protected static final int BOSS_3_SPEED = 10;
-	protected static final int BOSS_4_SPEED = 15;
-	protected static final int BOSS_5_SPEED = 15;
-	protected static final int BOSS_6_SPEED = 10;
+
+    /**
+     * constant boss life.
+     */
+    protected static final int BOSS_1_LIFE = 200;
+    protected static final int BOSS_2_LIFE = 150;
+    protected static final int BOSS_3_LIFE = 190;
+    protected static final int BOSS_4_LIFE = 100;
+    protected static final int BOSS_5_LIFE = 180;
+    protected static final int BOSS_6_LIFE = 300;
+
+    /**
+     * constant boss impact damage.
+     */
+    protected static final int BOSS_1_DAMAGE = 15;
+    protected static final int BOSS_2_DAMAGE = 10;
+    protected static final int BOSS_3_DAMAGE = 0;
+    protected static final int BOSS_4_DAMAGE = 0;
+    protected static final int BOSS_5_DAMAGE = 5;
+    protected static final int BOSS_6_DAMAGE = 5;
+
+    /**
+     * constant boss shoot delay in milliseconds.
+     */
+    protected static final long BOSS_1_SHOOT_DELAY = 50;
+    protected static final long BOSS_2_SHOOT_DELAY = 250;
+    protected static final long BOSS_3_SHOOT_DELAY = 300;
+    protected static final long BOSS_4_SHOOT_DELAY = 200;
+    protected static final long BOSS_5_SHOOT_DELAY = 250;
+    protected static final long BOSS_6_SHOOT_DELAY = 150;
+
+    /**
+     * constant boss speed.
+     */
+    protected static final int BOSS_1_SPEED = 20;
+    protected static final int BOSS_2_SPEED = 25;
+    protected static final int BOSS_3_SPEED = 10;
+    protected static final int BOSS_4_SPEED = 15;
+    protected static final int BOSS_5_SPEED = 15;
+    protected static final int BOSS_6_SPEED = 10;
 
 }
