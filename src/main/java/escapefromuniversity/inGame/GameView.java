07f6e339b9106d0e41fd37bd2308df817f9f7126
Daniel Capannini <daniel12capannini@gmail.com>
2022-06-24 10:09:35 +0200

    checkstyle in GameView

--- src/main/java/escapefromuniversity/inGame/GameView.java
+++ src/main/java/escapefromuniversity/inGame/GameView.java
@@ -1,12 +1,29 @@
 package escapefromuniversity.inGame;
 
+/**
+ * 
+ * interface of game view.
+ *
+ */
 public interface GameView {
-	
-	void update();
 
+    /**
+     * 
+     */
+    void update();
+
+    /**
+     * 
+     */
     void addPauseBG();
 
+    /**
+     * 
+     */
     void removePauseBG();
 
+    /**
+     * 
+     */
     void openMenu();
 }
