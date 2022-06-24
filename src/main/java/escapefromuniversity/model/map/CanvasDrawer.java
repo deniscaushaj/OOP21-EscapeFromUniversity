3495e489d098d9a295324c869d5787dae4156ffd
Denis Caushaj <91290254+deniscaushaj@users.noreply.github.com>
2022-06-24 22:27:46 +0200

    Improve drawImage method (random lines between tiles are no longer projected)

--- src/main/java/escapefromuniversity/model/map/CanvasDrawer.java
+++ src/main/java/escapefromuniversity/model/map/CanvasDrawer.java
@@ -5,11 +5,18 @@ package escapefromuniversity.model.map;
  */
 public interface CanvasDrawer {
 
+    public void clear();
+
+    public double getWidth();
+
+    public double getHeight();
+
     /**
      * Draws a tile.
+     *
      * @param filename the filename
      * @param imagePos the position in the file of the rectangle to draw
-     * @param drawPos the position in the map of the rectangle to draw
+     * @param drawPos  the position in the map of the rectangle to draw
      */
     void drawImage(String filename, Rectangle imagePos, Rectangle drawPos);
 }
--- src/main/java/escapefromuniversity/model/map/CanvasDrawerImpl.java
+++ src/main/java/escapefromuniversity/model/map/CanvasDrawerImpl.java
@@ -1,5 +1,7 @@
 package escapefromuniversity.model.map;
 
+import escapefromuniversity.model.basics.Point2D;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 
@@ -13,14 +15,16 @@ public class CanvasDrawerImpl implements CanvasDrawer {
 
     private final GraphicsContext gc;
     private final Map<String, Image> imagesCache;
+    private final Canvas canvas;
 
     /**
      * A constructor for CanvasDrawerImpl.
-     * @param gc the graphics of the canvas
+     * @param canvas the canvas used to draw
      */
-    public CanvasDrawerImpl(final GraphicsContext gc) {
-        this.gc = gc;
-        this.imagesCache = new HashMap<String, Image>();
+    public CanvasDrawerImpl(final Canvas canvas) {
+        this.canvas = canvas;
+        this.gc = canvas.getGraphicsContext2D();
+        this.imagesCache = new HashMap<>();
     }
 
     private Image getImageFromCacheOrLoad(final String filename) {
@@ -30,10 +34,24 @@ public class CanvasDrawerImpl implements CanvasDrawer {
         return imagesCache.get(filename);
     }
 
+    public void clear() {
+        this.gc.clearRect(0, 0, this.getWidth(), this.getHeight());
+    }
+
+    public double getWidth() {
+        return this.canvas.getWidth();
+    }
+
+    public double getHeight() {
+        return this.canvas.getHeight();
+    }
+
     @Override
     public void drawImage(final String filename, final Rectangle imagePos, final Rectangle drawPos) {
         this.gc.drawImage(this.getImageFromCacheOrLoad(filename),
-                imagePos.getMinX(), imagePos.getMinY(), imagePos.getWidth(), imagePos.getHeight(),
-                drawPos.getMinX(), drawPos.getMinY(), drawPos.getWidth(), drawPos.getHeight());
+                Math.round(imagePos.getMinX()), Math.round(imagePos.getMinY()),
+                Math.round(imagePos.getWidth()), Math.round(imagePos.getHeight()),
+                Math.round(drawPos.getMinX()-1), Math.round(drawPos.getMinY()-1),
+                Math.round(drawPos.getWidth()+2), Math.round(drawPos.getHeight()+2));
     }
 }
