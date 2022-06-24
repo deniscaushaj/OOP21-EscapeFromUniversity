cb261ec847811e59375b0bee2f15c2a918ca7ff3
Daniel Capannini <daniel12capannini@gmail.com>
2022-06-24 10:07:46 +0200

    checkstyle in GamenHUDPanel

--- src/main/java/escapefromuniversity/inGame/GameHUDPanel.java
+++ src/main/java/escapefromuniversity/inGame/GameHUDPanel.java
@@ -14,55 +14,71 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Toolkit;
 
-public class GameHUDPanel extends JLayeredPane{
-	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	private final JLabel creditsCounter = new JLabel();
-	private JProgressBar playerLifeBar;
-	private final Image creditsImage;
-	private final Rectangle playerLifeBarPosition;
-	private final Rectangle creditsPosition;
-	private final Rectangle creditsCounterPosition;
+/**
+ * 
+ * HUDPanel in game.
+ *
+ */
+public class GameHUDPanel extends JLayeredPane {
 
-	GameHUDPanel(final double windowRatio){
-		this.creditsCounter.setText("0");
-		this.creditsCounter.setFont(WindowSet.FONT);
-		this.creditsCounter.setForeground(Color.white);
-		this.creditsPosition = new Rectangle((int) (10 * windowRatio), (int) (50 * windowRatio), (int) (50 * windowRatio), (int) (50 * windowRatio));
-		this.creditsCounterPosition = new Rectangle((int) (60 * windowRatio), (int) (50 * windowRatio), (int) (50 * windowRatio), (int) (50 * windowRatio));
-		this.creditsCounter.setBounds(creditsCounterPosition);
-		this.creditsImage = new ImageIcon(OSFixes.getLocation("hud", "credits.png")).getImage().getScaledInstance((int) creditsPosition.getWidth(),
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    private final JLabel creditsCounter = new JLabel();
+    private JProgressBar playerLifeBar;
+    private final Image creditsImage;
+    private final Rectangle playerLifeBarPosition;
+    private final Rectangle creditsPosition;
+    private final Rectangle creditsCounterPosition;
+    GameHUDPanel(final double windowRatio) {
+        this.creditsCounter.setText("0");
+        this.creditsCounter.setFont(WindowSet.FONT);
+        this.creditsCounter.setForeground(Color.white);
+        this.creditsPosition = new Rectangle((int) (10 * windowRatio), (int) (50 * windowRatio), (int) (50 * windowRatio), (int) (50 * windowRatio));
+        this.creditsCounterPosition = new Rectangle((int) (60 * windowRatio), (int) (50 * windowRatio), (int) (50 * windowRatio), (int) (50 * windowRatio));
+        this.creditsCounter.setBounds(creditsCounterPosition);
+        this.creditsImage = new ImageIcon(OSFixes.getLocation("hud", "credits.png")).getImage().getScaledInstance((int) creditsPosition.getWidth(),
                 (int) creditsPosition.getHeight(), 
                 Image.SCALE_SMOOTH);
-		this.playerLifeBarPosition = new Rectangle((int) (10 * windowRatio), (int) (10 * windowRatio), (int) (300 * windowRatio), (int) (30 * windowRatio));
-		this.add(this.creditsCounter);
-		this.setOpaque(false);
-	}
-	
-	public void setLifeBar(final int maxLife) {
-		this.remove(this.playerLifeBar);
-		this.playerLifeBar = new JProgressBar(0, maxLife);
-		this.playerLifeBar.setBounds(this.playerLifeBarPosition);
-		this.playerLifeBar.setForeground(new Color(150, 0, 0));
-		this.add(this.playerLifeBar);
-	}
-	
-	public void updateCreditsCounter(final int credits) {
-		this.creditsCounter.setText("" + credits);
-	}
-	
-	public void updateLife(final int life) {
-		this.playerLifeBar.setValue(life);
-	}
-	
-	@Override
-	public void paintComponent(final Graphics g) {
-		super.paintComponent(g);
-		g.drawImage(this.creditsImage, (int) this.creditsPosition.getX(), (int) this.creditsPosition.getY(), null);
-		Toolkit.getDefaultToolkit().sync();
-	}
+        this.playerLifeBarPosition = new Rectangle((int) (10 * windowRatio), (int) (10 * windowRatio), (int) (300 * windowRatio), (int) (30 * windowRatio));
+        this.add(this.creditsCounter);
+        this.setOpaque(false);
+    }
+
+    /**
+     * 
+     * @param maxLife
+     */
+    public void setLifeBar(final int maxLife) {
+        this.remove(this.playerLifeBar);
+        this.playerLifeBar = new JProgressBar(0, maxLife);
+        this.playerLifeBar.setBounds(this.playerLifeBarPosition);
+        this.playerLifeBar.setForeground(new Color(150, 0, 0));
+        this.add(this.playerLifeBar);
+    }
+
+    /**
+     * 
+     * @param credits
+     */
+    public void updateCreditsCounter(final int credits) {
+        this.creditsCounter.setText("" + credits);
+    }
+
+    /**
+     * 
+     * @param life
+     */
+    public void updateLife(final int life) {
+        this.playerLifeBar.setValue(life);
+    }
+
+    @Override
+    public void paintComponent(final Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(this.creditsImage, (int) this.creditsPosition.getX(), (int) this.creditsPosition.getY(), null);
+        Toolkit.getDefaultToolkit().sync();
+    }
 
 }
