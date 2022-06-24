510ec7f478827dc8c8cedda02f5ee84854d5eb95
Denis Caushaj <91290254+deniscaushaj@users.noreply.github.com>
2022-06-24 22:33:36 +0200

    Create PlayerCameraImpl class

---
+++ src/main/java/escapefromuniversity/model/map/PlayerCameraImpl.java
@@ -0,0 +1,23 @@
+package escapefromuniversity.model.map;
+
+import escapefromuniversity.model.basics.Point2D;
+import escapefromuniversity.model.gameObject.player.Player;
+
+public class PlayerCameraImpl implements Camera {
+
+    private static final double RADIUS = 10;
+    private final Player player;
+
+    public PlayerCameraImpl(Player player){
+        this.player = player;
+    }
+
+    @Override
+    public Rectangle calcMapProjection() {
+        var hb = this.player.getObjectHitBox();
+        var center = hb.getBottomLeftCorner().sum(hb.getUpperRightCorner()).multiplication(0.5);
+        return new Rectangle(center.sum(new Point2D(-RADIUS, -RADIUS)), center.sum(new Point2D(RADIUS, RADIUS)));
+    }
+
+
+}
