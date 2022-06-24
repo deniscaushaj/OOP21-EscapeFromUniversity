1b3fa2f481bd5b40e0b69691abf4aee43bdb71ac
MarcoAntolini <marco.antolini2001@gmail.com>
2022-06-24 13:40:58 +0200

    refactor
    Co-Authored-By: DanielCapannini <93661927+DanielCapannini@users.noreply.github.com>

--- src/main/java/escapefromuniversity/inGame/Sprite.java
+++ src/main/java/escapefromuniversity/inGame/Sprite.java
@@ -1,1 +1,17 @@
+package escapefromuniversity.inGame;
+
+import java.awt.*;
+
+import escapefromuniversity.model.gameObject.State;
+
+public interface Sprite {
+
     boolean checkUpdate();
+
+    void setImage();
+
+    Image getImage();
+    
+    void setState(State state);
+
+}
--- src/main/java/escapefromuniversity/inGame/SpriteImpl.java
+++ src/main/java/escapefromuniversity/inGame/SpriteImpl.java
@@ -1,3 +1,32 @@
+package escapefromuniversity.inGame;
+
+import escapefromuniversity.model.gameObject.GameObjectType;
+import escapefromuniversity.model.gameObject.State;
+import escapefromuniversity.utilities.OSFixes;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
+public class SpriteImpl implements Sprite {
+
+    private State state;
+    private final GameObjectType objType;
+    private Image image;
+    private File filepath;
+    private File prevFilepath;
+    private String folder;
+    private String filename;
+    private int tileCount;
+    private int lastUpdate;
+
+    public SpriteImpl(State state, GameObjectType objType) {
+        this.state = state;
+        this.objType = objType;
+        this.tileCount = 1;
+    }
+
     @Override
     public boolean checkUpdate() {
         final long currentTime = System.currentTimeMillis();
@@ -35,9 +64,89 @@
             e.printStackTrace();
         }
     }
+
+    @Override
+    public Image getImage() {
+        return this.image;
+    }
+
     private void setupSpriteFilepath() {
         this.setFilename(this.getFolder());
         this.setSpriteDirection();
         this.setSpriteNumber();
         this.setPrevFilepath(this.getFilepath());
     }
+
+    private void setFilepath() {
+        this.filepath = new File(OSFixes.getLocation(this.getFolder(), this.getFilename()));
+    }
+
+    private File getFilepath() {
+        return this.filepath;
+    }
+
+    private void setPrevFilepath(File prevFilepath) {
+        this.prevFilepath = prevFilepath;
+    }
+
+    private File getPrevFilepath() {
+        return this.prevFilepath;
+    }
+
+    private void setFolder(String folder) {
+        this.folder = folder;
+    }
+
+    private String getFolder() {
+        return this.folder;
+    }
+
+    private void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    private String getFilename() {
+        return this.filename;
+    }
+
+    private void setSpriteDirection() {
+        switch (this.state) {
+            case UP:
+                this.setFilename(this.getFilename().concat("_back"));
+                break;
+            case DOWN:
+                this.setFilename(this.getFilename().concat("_front"));
+                break;
+            case RIGHT:
+                this.setFilename(this.getFilename().concat("_right"));
+                break;
+            case LEFT:
+                this.setFilename(this.getFilename().concat("_left"));
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void setSpriteNumber() {
+        switch(this.tileCount) {
+            case 1:
+                this.setFilename(this.getFilename().concat("_1"));
+                this.tileCount++;
+            case 2:
+                this.setFilename(this.getFilename().concat("_2"));
+                this.tileCount++;
+            case 3:
+                this.setFilename(this.getFilename().concat("_3"));
+                this.tileCount = 1;
+            default: {
+            }
+        }
+    }
+
+	@Override
+	public void setState(State state) {
+		this.state = state;
+	}
+
+}
