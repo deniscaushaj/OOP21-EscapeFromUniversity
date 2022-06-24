bc78b1b2a98982c24895fbfb7202e11f863e45be
Denis Caushaj <91290254+deniscaushaj@users.noreply.github.com>
2022-06-24 22:33:08 +0200

    Edit constructor

--- src/main/java/escapefromuniversity/model/map/ObstacleObject.java
+++ src/main/java/escapefromuniversity/model/map/ObstacleObject.java
@@ -9,19 +9,13 @@ import escapefromuniversity.model.gameObject.GameObjectType;
  * A class which models an ObstacleStaticObject.
  */
 public class ObstacleObject extends AbstractStaticGameObject {
-
-    public ObstacleObject(GameObjectType type, Point2D position, Point2D upperCorner, Room room) {
-        super(type, position, upperCorner, room);
-    }
-
     /**
      * A constructor for an ObstacleObject.
      * @param type the type of GameObject
      * @param position the upper-left corner
-     * @param byteValue the byte value of the object
      */
-    public ObstacleObject(final GameObjectType type, final Rectangle position, final Byte byteValue) {
-        super(type, position, byteValue);
+    public ObstacleObject(final GameObjectType type, final Rectangle position) {
+        super(type, position.getBottomLeft(), position.getTopRight(), null);
     }
 
     @Override
