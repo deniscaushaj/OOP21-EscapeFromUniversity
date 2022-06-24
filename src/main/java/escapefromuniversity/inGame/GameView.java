2d97d2d2e060979377ba77792caa36878370eda0
Daniel Capannini <daniel12capannini@gmail.com>
2022-06-24 14:56:08 +0200

    in GameView new metodes remuveSpriteAnimation,updateSpriteAnimation and addSpriteAnimation

--- src/main/java/escapefromuniversity/inGame/GameView.java
+++ src/main/java/escapefromuniversity/inGame/GameView.java
@@ -1,6 +1,9 @@
 package escapefromuniversity.inGame;
 
 import escapefromuniversity.model.GameState;
+import escapefromuniversity.model.basics.Point2D;
+import escapefromuniversity.model.gameObject.GameObjectType;
+import escapefromuniversity.model.gameObject.State;
 
 /**
  * 
@@ -29,5 +32,33 @@ public interface GameView {
      */
     void openMenu();
 
+    /**
+     * 
+     * @param gameState
+     */
     void end(GameState gameState);
+
+    /**
+     * 
+     * @param id
+     */
+    void remuveSpriteAnimation(int id);
+
+    /**
+     * 
+     * @param id
+     * @param position
+     * @param deltaTime
+     */
+    void updateSpriteAnimation(int id, Point2D position, long deltaTime);
+
+    /**
+     * 
+     * @param id
+     * @param state
+     * @param type
+     * @param height
+     * @param width
+     */
+    void addSpriteAnimation(int id, State state, GameObjectType type, int height, int width);
 }
--- src/main/java/escapefromuniversity/inGame/GameViewImpl.java
+++ src/main/java/escapefromuniversity/inGame/GameViewImpl.java
@@ -2,6 +2,9 @@ package escapefromuniversity.inGame;
 
 import escapefromuniversity.launcher.LauncherView;
 import escapefromuniversity.model.GameState;
+import escapefromuniversity.model.basics.Point2D;
+import escapefromuniversity.model.gameObject.GameObjectType;
+import escapefromuniversity.model.gameObject.State;
 import escapefromuniversity.utilities.OSFixes;
 import escapefromuniversity.utilities.WindowSet;
 
@@ -18,106 +21,121 @@ import static escapefromuniversity.utilities.LauncherResizer.screenWidth;
 
 public class GameViewImpl extends JFrame implements GameView, KeyListener {
 
-	private GameController gameController;
-	private final JFrame window;
-	private JPanel pause;
-	private BufferedImage tempScreen;
-	private final GameHUDPanel gameHUD = new GameHUDPanel(WindowSet.getWindowRatio());
-	private static final long DELAY_CLOSE = 5000;
-	private final Map<Integer,Sprite> animations = new HashMap<>();
+    private GameController gameController;
+    private final JFrame window;
+    private JPanel pause;
+    private BufferedImage tempScreen;
+    private final GameHUDPanel gameHUD = new GameHUDPanel(WindowSet.getWindowRatio());
+    private static final long DELAY_CLOSE = 5000;
+    private final Map<Integer,SpriteAnimation> spriteAnimations = new HashMap<>();
 
-	public GameViewImpl(GameController gameController) {
-		this.gameController = gameController;
-		this.window = new JFrame();
-		this.window.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		this.window.setUndecorated(true);
-		this.window.setSize(screenWidth, screenHeight);
-		this.window.setResizable(false);
-		this.window.setTitle("Escape From University");
-		String logo = OSFixes.getLocation("images", "logo.png"); // TODO change icon
-		this.window.setIconImage(Toolkit.getDefaultToolkit().getImage(logo));
-		this.window.setVisible(true);
-		//	private final JPanel gamePanel;
+    public GameViewImpl(GameController gameController) {
+        this.gameController = gameController;
+        this.window = new JFrame();
+        this.window.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.window.setUndecorated(true);
+        this.window.setSize(screenWidth, screenHeight);
+        this.window.setResizable(false);
+        this.window.setTitle("Escape From University");
+        String logo = OSFixes.getLocation("images", "logo.png"); // TODO change icon
+        this.window.setIconImage(Toolkit.getDefaultToolkit().getImage(logo));
+        this.window.setVisible(true);
+        //	private final JPanel gamePanel;
 
-		this.window.addKeyListener(this);
+        this.window.addKeyListener(this);
 
-		this.pause = new JPanel();
-		this.tempScreen = new BufferedImage(screenWidth, screenHeight, BufferedImage.TYPE_INT_ARGB);
-		Graphics2D g2d = this.tempScreen.createGraphics();
-		g2d.setFont(WindowSet.FONT);
+        this.pause = new JPanel();
+        this.tempScreen = new BufferedImage(screenWidth, screenHeight, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g2d = this.tempScreen.createGraphics();
+        g2d.setFont(WindowSet.FONT);
 
-		drawToScreen();
-		drawToImage();
-	}
+        drawToScreen();
+        drawToImage();
+    }
 
-	public void drawToScreen() {
-		Graphics g = this.window.getGraphics();
-		g.drawImage(this.tempScreen, 0, 0, screenWidth, screenHeight, null);
-		
-//		g.dispose();
-	}
+    public void drawToScreen() {
+        Graphics g = this.window.getGraphics();
+        g.drawImage(this.tempScreen, 0, 0, screenWidth, screenHeight, null);
 
-	public void drawToImage () {
-		// camera, hud, dynamic
+        //		g.dispose();
+    }
 
-	}
+    public void drawToImage () {
+        // camera, hud, dynamic
 
-	@Override
-	public void update() {
+    }
 
-	}
+    @Override
+    public void update() {
 
-	@Override
-	public void addPauseBG() {
-		Color color = new Color(0,0,0,205);
-		this.pause.setBackground(color);
-		this.window.add(this.pause);
-	}
+    }
 
-	@Override
-	public void removePauseBG() {
-		this.window.remove(this.pause);
-	}
+    @Override
+    public void addPauseBG() {
+        Color color = new Color(0, 0, 0, 205);
+        this.pause.setBackground(color);
+        this.window.add(this.pause);
+    }
 
-	@Override
-	public void keyTyped(KeyEvent key) {
-	}
+    @Override
+    public void removePauseBG() {
+        this.window.remove(this.pause);
+    }
 
-	@Override
-	public void keyPressed(KeyEvent key) {
-		if(this.gameController != null) {
-			this.gameController.pressKey(key);
-		}
-	}
+    @Override
+    public void keyTyped(final KeyEvent key) {
+    }
 
-	@Override
-	public void keyReleased(KeyEvent key) {
-		if(this.gameController != null) {
-			this.gameController.releaseKey(key);
-		}
-	}
+    @Override
+    public void keyPressed(final KeyEvent key) {
+        if(this.gameController != null) {
+            this.gameController.pressKey(key);
+        }
+    }
 
-	@Override
-	public void openMenu() {
-		this.gameController.setGameState(GameState.MENU);
-	}
+    @Override
+    public void keyReleased(final KeyEvent key) {
+        if (this.gameController != null) {
+            this.gameController.releaseKey(key);
+        }
+    }
 
-	@Override
-	public void end(GameState gameState) {
-		if(gameState == GameState.WIN) {
-			//aggiorna con immagine vittoria
-		}else if(gameState == GameState.LOST){
-			//aggiorna con immagine sconfitta
-		}
-		try {
-			Thread.sleep(DELAY_CLOSE);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		LauncherView.createLauncher();
-//		System.exit(0);
-		this.window.setVisible(false);
-		this.window.dispose();
-	}
-	
+    @Override
+    public void openMenu() {
+        this.gameController.setGameState(GameState.MENU);
+    }
+
+    @Override
+    public void end(final GameState gameState) {
+        if (gameState == GameState.WIN){
+            //aggiorna con immagine vittoria
+        } else if (gameState == GameState.LOST){
+            //aggiorna con immagine sconfitta
+        }
+        try {
+            Thread.sleep(DELAY_CLOSE);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        LauncherView.createLauncher();
+        //		System.exit(0);
+        this.window.setVisible(false);
+        this.window.dispose();
+    }
+
+    public void remuveSpriteAnimation(int id) {
+        this.spriteAnimations.remove(id);
+    }
+    
+    public void updateSpriteAnimation(int id, Point2D position, long deltaTime) {
+        this.spriteAnimations.get(id).setPosition(position);
+        this.spriteAnimations.get(id).getSprite();
+    }
+    
+    public void addSpriteAnimation(int id, State state, GameObjectType type, int height, int width) {
+        final Sprite sprite = new SpriteImpl(state, type);
+        final SpriteAnimation animation = new SpriteAnimation(sprite, height, width);
+        this.spriteAnimations.put(id, animation);
+    }
+
 }
