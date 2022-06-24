38519928f29a4edc3f78d358c51ca7d74e291edf
MarcoAntolini <marco.antolini2001@gmail.com>
2022-06-24 13:40:46 +0200

    add method to get also bullets sprite

--- src/main/java/escapefromuniversity/inGame/SpriteImpl.java
+++ src/main/java/escapefromuniversity/inGame/SpriteImpl.java
@@ -9,3 +9,35 @@
         return false;
     }
 
+    @Override
+    public void setImage() {
+        if(this.objType.equals(GameObjectType.BOSS1) || this.objType.equals(GameObjectType.BOSS2)
+                || this.objType.equals(GameObjectType.BOSS3) || this.objType.equals(GameObjectType.BOSS4)
+                || this.objType.equals(GameObjectType.BOSS5) || this.objType.equals(GameObjectType.BOSS6)) {
+            this.setFolder("boss");
+            this.setupSpriteFilepath();
+        } else if(this.objType.equals(GameObjectType.PLAYER)) {
+            this.setFolder("player");
+            this.setupSpriteFilepath();
+        } else if(this.objType.equals(GameObjectType.BULLET_BOSS_1) || this.objType.equals(GameObjectType.BULLET_BOSS_2)
+                || this.objType.equals(GameObjectType.BULLET_BOSS_3) || this.objType.equals(GameObjectType.BULLET_BOSS_4)
+                || this.objType.equals(GameObjectType.BULLET_BOSS_5) || this.objType.equals(GameObjectType.BULLET_BOSS_6)) {
+            this.setFolder("bullet");
+            this.setFilename("boss_bullet.png");
+        } else if(this.objType.equals(GameObjectType.BULLET_PLAYER)) {
+            this.setFolder("bullet");
+            this.setFilename("player_bullet.png");
+        }
+        this.setFilepath();
+        try {
+            this.image = ImageIO.read(this.getFilepath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private void setupSpriteFilepath() {
+        this.setFilename(this.getFolder());
+        this.setSpriteDirection();
+        this.setSpriteNumber();
+        this.setPrevFilepath(this.getFilepath());
+    }
