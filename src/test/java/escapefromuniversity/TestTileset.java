dee65a36cdbca65832845a257d1417894209477b
Denis Caushaj <denis.caushaj@studio.unibo.it>
2022-06-26 16:25:21 +0200

    Add JUnit tests

---
+++ src/test/java/escapefromuniversity/TestTileset.java
@@ -0,0 +1,41 @@
+package escapefromuniversity;
+
+import escapefromuniversity.model.map.*;
+import javafx.scene.canvas.Canvas;
+import org.junit.jupiter.api.Test;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class TestTileset {
+
+    MapProperties mapProp;
+
+    public TestTileset() throws ParserConfigurationException, IOException, SAXException {
+        this.mapProp = new TMXMapParser("final-map.tmx").parse();
+    }
+
+    @Test
+    public void testTilesetName() {
+        List<String> tilesetNames = mapProp.getTilesets().stream().map(Tileset::getFileName).toList();
+        assertTrue(tilesetNames.contains("tilesets/Characters_MV.png"));
+        assertTrue(tilesetNames.contains("tilesets/grass.png"));
+        assertTrue(tilesetNames.contains("tilesets/Room_Builder_free_48x48.png"));
+        assertFalse(tilesetNames.contains("tilesets/grass.tsx"));
+        assertFalse(tilesetNames.contains("final-map.tmx"));
+        assertFalse(tilesetNames.contains(".png"));
+    }
+
+    @Test
+    public void testSearchTileset() {
+        TileDrawer td = new TileDrawerImpl(mapProp, new CanvasDrawerImpl(new Canvas()));
+        assertEquals("tilesets/Room_Builder_free_48x48.png", td.searchTileset(1).getFileName());
+        assertEquals("tilesets/Characters_MV.png", td.searchTileset(2007).getFileName());
+        assertNotEquals("tilesets/Characters_MV.png", td.searchTileset(2008).getFileName());
+    }
+}
