f889ea5f06ae1a079cd387d380e7b1542f18ad56
Daniel Capannini <daniel12capannini@gmail.com>
2022-06-26 15:18:00 +0200

    sistem checkstyle in ControllerView

--- src/main/java/escapefromuniversity/inGame/ControllerView.java
+++ src/main/java/escapefromuniversity/inGame/ControllerView.java
@@ -5,18 +5,53 @@ import escapefromuniversity.model.basics.Point2D;
 import escapefromuniversity.model.gameObject.GameObjectType;
 import escapefromuniversity.model.gameObject.State;
 
+/**
+ * 
+ * interface of controller of game view.
+ *
+ */
 public interface ControllerView {
 
+    /**
+     * 
+     */
     void updateView();
 
+    /**
+     * 
+     * @param id : id of object.
+     */
     void removeSpriteAnimation(int id);
 
+    /**
+     * 
+     * @param id : id of object.
+     * @param position : new position.
+     * @param state
+     */
     void updateSpriteAnimation(int id, Point2D position, State state);
 
+    /**
+     * 
+     * @param id : id of object.
+     * @param state : state of object.
+     * @param type : type of object.
+     * @param height
+     * @param width 
+     */
     void addSpriteAnimation(int id, State state, GameObjectType type, int height, int width);
 
+    /**
+     * 
+     * @param id : id of object.
+     * @return if the id is present.
+     */
     boolean containThisID(int id);
 
+    /**
+     * 
+     * @param gameState : win or lost.
+     */
     void end(GameState gameState);
 
 }
