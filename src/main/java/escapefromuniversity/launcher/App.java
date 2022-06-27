b884d71b7635e14387b36225875ba8202e1e34cd
MarcoAntolini <marco.antolini2001@gmail.com>
2022-06-27 15:37:25 +0200

    add javadoc

--- src/main/java/escapefromuniversity/launcher/App.java
+++ src/main/java/escapefromuniversity/launcher/App.java
@@ -1,13 +1,15 @@
 package escapefromuniversity.launcher;
 /**
- * 
- * app.
- *
+ * The main class from which starts the game.
  */
 public final class App {
 
 	private App() { }
 
+	/**
+	 * Starts the launcher of the game.
+	 * @param args
+	 */
 	public static void main (final String[] args) {
 		LauncherView.createLauncher();
 	}
