4317ef054340c1bb2d3e611c21f165a71680ff7f
Daniel Capannini <daniel12capannini@gmail.com>
2022-06-27 12:06:46 +0200

    fixes checkstyle in GameState

--- src/main/java/escapefromuniversity/model/GameState.java
+++ src/main/java/escapefromuniversity/model/GameState.java
@@ -1,30 +1,35 @@
 package escapefromuniversity.model;
 
+/**
+ * 
+ * enum represents the game states.
+ *
+ */
 public enum GameState {
-    
+
     /**
      * Identifies the menu state.
-     */    
+     */
     MENU,
     /**
      * Identifies the play state.
-     */   
+     */
     PLAY,
     /**
      * Identifies the quiz state.
-     */   
+     */
     QUIZ,
     /**
      * Identifies the fight state.
-     */   
+     */
     FIGHT,
     /**
      * Identifies the graduated state.
-     */   
+     */
     GRADUATED,
     /**
      * Identifies the shop state.
-     */   
+     */
     SHOP,
     /**
      * Identifies the lost state.
@@ -32,6 +37,6 @@ public enum GameState {
     LOST,
     /**
      * Identifies the win state.
-     */   
+     */
     WIN;
 }
