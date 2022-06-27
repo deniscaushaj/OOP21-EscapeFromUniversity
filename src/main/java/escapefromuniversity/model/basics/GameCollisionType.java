9c4992cc79738f4968b018c2cde1cb191899e64f
Daniel Capannini <daniel12capannini@gmail.com>
2022-06-27 09:52:24 +0200

    system checkstyle in GameCollisionType

--- src/main/java/escapefromuniversity/model/basics/GameCollisionType.java
+++ src/main/java/escapefromuniversity/model/basics/GameCollisionType.java
@@ -1,27 +1,32 @@
 package escapefromuniversity.model.basics;
 
+/**
+ * 
+ * enum types of collisions.
+ *
+ */
 public enum GameCollisionType {
-	
-	/**
-	 * Identifies an obstacle object.
-	 */
-	OBSTACLE,
-	/**
-	 * Identifies a boss or the player.
-	 */
-	ENTITY,
-	/**
-	 * Identifies an immune boss or player.
-	 */
-	IMMUNE_ENTITY,
-	/**
-	 * Identifies a bullet.
-	 */
-	BULLET,
-	/**
-	 * Identifies an immune bullet.
-	 */
-	IMMUNE_BULLET;
-	
+
+    /**
+     * Identifies an obstacle object.
+     */
+    OBSTACLE,
+    /**
+     * Identifies a boss or the player.
+     */
+    ENTITY,
+    /**
+     * Identifies an immune boss or player.
+     */
+    IMMUNE_ENTITY,
+    /**
+     * Identifies a bullet.
+     */
+    BULLET,
+    /**
+     * Identifies an immune bullet.
+     */
+    IMMUNE_BULLET;
+
 
 }
