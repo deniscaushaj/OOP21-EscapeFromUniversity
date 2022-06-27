6120a9a9aff8791bc97fdd789c99da3c9632b3c6
Daniel Capannini <daniel12capannini@gmail.com>
2022-06-27 09:28:35 +0200

    in Vector2D new metode normal

--- src/main/java/escapefromuniversity/model/basics/Vector2D.java
+++ src/main/java/escapefromuniversity/model/basics/Vector2D.java
@@ -1,17 +1,45 @@
 package escapefromuniversity.model.basics;
 
-public class Vector2D extends Point2D{
+/**
+ * 
+ * class vector2D extend point2D.
+ *
+ */
+public class Vector2D extends Point2D {
 
-	public Vector2D(double x, double y) {
-		super(x, y);
-	}
-	
-	public Vector2D(final Vector2D vector) {
-		super(vector.getX(), vector.getY());
-	}
-	
-	public Vector2D multiplication(final double mul) {
-		return new Vector2D(this.getX()*mul, this.getY()*mul);
-	}
+    /**
+     * 
+     * @param x
+     * @param y
+     */
+    public Vector2D(final double x, final double y) {
+        super(x, y);
+    }
+
+    /**
+     * 
+     * @param vector
+     */
+    public Vector2D(final Vector2D vector) {
+        super(vector.getX(), vector.getY());
+    }
+
+    /**
+     * 
+     * @param mul : multiplicative factor.
+     * @return new Vector2D.
+     */
+    public Vector2D multiplication(final double mul) {
+        return new Vector2D(this.getX() * mul, this.getY() * mul);
+    }
+
+    /**
+     * 
+     * @return new Vector2D with module 1.
+     */
+    public Vector2D normal() {
+        final double module = Math.sqrt(Math.pow(getX(), 2) + Math.pow(getY(), 2));
+        return new Vector2D(this.getX() / module, this.getY() / module);
+    }
 
 }
