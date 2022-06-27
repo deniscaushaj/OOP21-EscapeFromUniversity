d2e23144c8b1bda02ceb34099d45e45ca315d0b9
MarcoAntolini <marco.antolini2001@gmail.com>
2022-06-27 15:38:36 +0200

    fix javadoc and remove comments

--- src/main/java/escapefromuniversity/utilities/LauncherResizer.java
+++ src/main/java/escapefromuniversity/utilities/LauncherResizer.java
@@ -6,40 +6,28 @@ import javafx.stage.Screen;
  * The window resizer based on the resolution.
  */
 public class LauncherResizer {
-	
-	/**
-	 * The screen width.
-	 */
+
 	public static final int SCREENWIDTH = (int) Screen.getPrimary().getBounds().getWidth();
-	/**
-	 * The screen height.
-	 */
 	public static final int SCREENHEIGHT = (int) Screen.getPrimary().getBounds().getHeight();
-	/**
-	 * The scene width.
-	 */
 	public static int sceneWidth = 0;
-	/**
-	 * The scene height.
-	 */
 	public static int sceneHeight = 0;
 
 	/**
-	 * Set the scene size.
-	 * @param screenWidth The screen width.
-	 * @param screenHeight The screen height.
+	 * Sets the scene size.
+	 * @param screenWidth the screen width.
+	 * @param screenHeight the screen height.
 	 */
 	public void setSceneSize(final int screenWidth, final int screenHeight) {
-		if (screenWidth <= 1280) { //HD 1280 x 720
+		if (screenWidth <= 1280) {
 			sceneWidth = 480;
 			sceneHeight = 320;
-		} else if (screenWidth <= 1920) { //FHD 1920 x 1080
+		} else if (screenWidth <= 1920) {
 			sceneWidth = 600;
 			sceneHeight = 400;
-		} else if (screenWidth <= 2560) { //QHD 2560 x 1440
+		} else if (screenWidth <= 2560) {
 			sceneWidth = 900;
 			sceneHeight = 600;
-		} else if (screenWidth >= 3840) { //UHD 3840 x 2160
+		} else if (screenWidth >= 3840) {
 			sceneWidth = 1200;
 			sceneHeight = 800;
 		}
