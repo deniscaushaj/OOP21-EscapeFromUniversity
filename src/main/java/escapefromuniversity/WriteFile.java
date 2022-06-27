c89c62560455d1eea12abe820578f2de04cf7a7c
MarcoAntolini <marco.antolini2001@gmail.com>
2022-06-27 15:38:16 +0200

    add javadoc

--- src/main/java/escapefromuniversity/WriteFile.java
+++ src/main/java/escapefromuniversity/WriteFile.java
@@ -6,17 +6,31 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
 
+/**
+ * A utility class to write into a file.
+ */
 public class WriteFile {
 
 	private final String fileName;
 	private final BufferedWriter writer;
 	private ArrayList<String> list;
 
+	/**
+	 * Instantiates the object.
+	 * @param folderName the folder of the file to write into.
+	 * @param fileName the name of the file to write into.
+	 * @throws IOException
+	 */
 	public WriteFile(String folderName, String fileName) throws IOException {
 		this.fileName = OSFixes.getLocation(folderName, fileName);
 		this.writer = new BufferedWriter(new FileWriter(this.fileName));
 	}
 
+	/**
+	 * Reads a file, writes a new line into it and sorts its lines.
+	 * @param newLine a line to write into the file.
+	 * @throws IOException
+	 */
 	public void writeNewLine(String newLine) throws IOException {
 		BufferedReader reader = new BufferedReader(new FileReader(this.fileName));
 		this.list = new ArrayList<>();
@@ -34,6 +48,10 @@ public class WriteFile {
 		reader.close();
 	}
 
+	/**
+	 * Closes the buffered writer.
+	 * @throws IOException
+	 */
 	public void close() throws IOException {
 		this.writer.close();
 	}
