7274754b9a947e0b280dc870c0c3eb0708f7c486
MarcoAntolini <marco.antolini2001@gmail.com>
2022-06-27 15:36:41 +0200

    add javadoc

--- src/main/java/escapefromuniversity/input/Command.java
+++ src/main/java/escapefromuniversity/input/Command.java
@@ -1,29 +1,57 @@
 package escapefromuniversity.input;
 
+/**
+ * A class to create an object containing a key and some properties of it.
+ * @param <Key>
+ * @param <Clicked>
+ * @param <Direction>
+ */
 public class Command<Key, Clicked, Direction> {
 
     private final Key key;
     private Clicked clicked;
     private final Direction direction;
 
+    /**
+     * Instantiates this object.
+     * @param key
+     * @param clicked
+     * @param direction
+     */
     public Command(final Key key, final Clicked clicked, final Direction direction) {
         this.key = key;
         this.clicked = clicked;
         this.direction = direction;
     }
 
+    /**
+     * Returns the key.
+     * @return the key.
+     */
     public Key getKey() {
         return this.key;
     }
 
+    /**
+     * Returns the clicked.
+     * @return the clicked.
+     */
     public Clicked getClicked() {
         return this.clicked;
     }
 
+    /**
+     * Returns the direction.
+     * @return the direction.
+     */
     public Direction getDirection() {
         return this.direction;
     }
 
+    /**
+     * Sets clicked.
+     * @param clicked the clicked to set.
+     */
     public void setClicked(final Clicked clicked) {
         this.clicked = clicked;
     }
