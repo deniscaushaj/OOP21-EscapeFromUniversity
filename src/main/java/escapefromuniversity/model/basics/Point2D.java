99d864ac95f73e3a49113f9849ea39a8fb7bcc89
Denis Caushaj <91290254+deniscaushaj@users.noreply.github.com>
2022-06-28 10:30:28 +0200

    Add toString method in Point2D

--- src/main/java/escapefromuniversity/model/basics/Point2D.java
+++ src/main/java/escapefromuniversity/model/basics/Point2D.java
@@ -104,5 +104,11 @@ public class Point2D {
     }
 
 
-
+    @Override
+    public String toString() {
+        return "Point2D{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
 }
