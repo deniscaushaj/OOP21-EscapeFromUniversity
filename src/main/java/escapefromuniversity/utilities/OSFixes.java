257f1275aeb9be900ff998a8d22fd5ccda21dde2
MarcoAntolini <marco.antolini2001@gmail.com>
2022-06-29 16:17:38 +0200

    create new methods to create and access an external folder and file from the project

--- src/main/java/escapefromuniversity/utilities/OSFixes.java
+++ src/main/java/escapefromuniversity/utilities/OSFixes.java
@@ -1,6 +1,8 @@
 package escapefromuniversity.utilities;
 
 import java.awt.Desktop;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -11,6 +13,7 @@ import java.net.URISyntaxException;
 public class OSFixes {
 
     public final static String FS = System.getProperty("file.separator");
+    public final static String UH = System.getProperty("user.home");
     public final static String UD = System.getProperty("user.dir");
     private final static String OS = System.getProperty("os.name").toLowerCase();
 
@@ -43,12 +46,30 @@ public class OSFixes {
 
     /**
      * Getter method to obtain a string which contains the filepath of the file to open from resources folder. 
-     * @param folderName the name of the subfolder inside resources folder to look into.
+     * @param folderName the name of the subfolder inside res folder to look into.
      * @param fileName the name of the file to open.
      * @return a string containing the filepath.
      */
     public static String getLocation(String folderName, String fileName) {
         return UD + FS + "src" + FS + "main" + FS + "resources" + FS + folderName + FS + fileName;
+    }
+
+    public static String getExternalLocation(String folderName, String fileName) {
+        return UH + FS + folderName + FS + fileName;
+    }
+
+    public static void createHomeFolder() throws IOException {
+        File folder = new File(UH + FS + "EscapeFromUniversity");
+        if (!folder.exists() && !folder.mkdirs()) {
+            throw new IOException();
+        }
+    }
+
+    public static void createScoreFile() throws IOException {
+        File scores = new File(OSFixes.getExternalLocation("EscapeFromUniversity", "score.txt"));
+        if (!scores.exists() && !scores.createNewFile()) {
+            throw new IOException();
+        }
     }
 
 }
