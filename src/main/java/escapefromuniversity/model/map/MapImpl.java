aae44054e765329d7eff6eab10f34f1c94564967
Daniel Capannini <daniel12capannini@gmail.com>
2022-06-29 08:26:39 +0200

    in MapImpl implement metode getPlayer

--- src/main/java/escapefromuniversity/model/map/MapImpl.java
+++ src/main/java/escapefromuniversity/model/map/MapImpl.java
@@ -4,9 +4,11 @@ import java.util.LinkedList;
 import java.util.List;
 
 import escapefromuniversity.model.GameInit;
+import escapefromuniversity.model.IteretorOfID;
 import escapefromuniversity.model.gameObject.enemy.Boss;
 import escapefromuniversity.model.gameObject.DynamicGameObject;
 import escapefromuniversity.model.gameObject.GameObject;
+import escapefromuniversity.model.gameObject.GameObjectType;
 import escapefromuniversity.model.gameObject.StaticGameObject;
 import escapefromuniversity.model.gameObject.player.Player;
 
@@ -15,12 +17,13 @@ public class MapImpl implements GameInit {
     private final List<DynamicGameObject> dynamicGameObjects = new LinkedList<>();
     private final MapManager mapManager;
     private final MapProperties mapProperties;
+    private IteretorOfID iterator = new IteretorOfID();
 
     private final static String TMX_MAP_PATH = "final-map.tmx";
 
 
     public MapImpl(final MapManager mapManager) {
-    	this.mapManager = mapManager;
+        this.mapManager = mapManager;
         this.mapProperties = new TMXMapParser(TMX_MAP_PATH).parse();
     }
 
@@ -28,14 +31,16 @@ public class MapImpl implements GameInit {
     @Override
     public void update(final double deltaTime) {
         final List<DynamicGameObject> listDGObj = new LinkedList<>(List.copyOf(this.dynamicGameObjects));
-        /*listDGObj.iterator().forEachRemaining(obj -> { obj.update(deltaTime);});
+        listDGObj.iterator().forEachRemaining(obj -> { 
+            obj.update(deltaTime);
+        });
         listDGObj.iterator().forEachRemaining(obj -> {
             for (final GameObject obj2 : this.getAllGameObject()) {
                 obj.collisionWith(obj2);
             }
-        });*/
+        });
     }
-    
+
     private List<GameObject> getAllGameObject() {
         final List<GameObject> obj = new LinkedList<>(staticGameObjects);
         obj.addAll(dynamicGameObjects);
@@ -51,12 +56,14 @@ public class MapImpl implements GameInit {
 
     @Override
     public void addDynamicGameObject(final DynamicGameObject dGObj) {
+        dGObj.setID(iterator.next());
         dynamicGameObjects.add(dGObj);
 
     }
 
     @Override
     public void addStaticGameObject(final StaticGameObject sGObj) {
+        sGObj.setID(iterator.next());
         staticGameObjects.add(sGObj);
 
     }
@@ -66,22 +73,21 @@ public class MapImpl implements GameInit {
         return this.dynamicGameObjects;
     }
 
-	@Override
-	public Player getPlayer() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public Player getPlayer() {
+        return (Player) this.dynamicGameObjects.stream().filter(e -> e.getType() == GameObjectType.PLAYER).findAny().orElse(null);
+    }
 
 
-	@Override
-	public void goQuiz(final Boss boss) {
-		this.getMapManager().setupQuiz(boss);
-	}
+    @Override
+    public void goQuiz(final Boss boss) {
+        this.getMapManager().setupQuiz(boss);
+    }
 
-	@Override
-	public MapManager getMapManager() {
-		return this.mapManager;
-	}
+    @Override
+    public MapManager getMapManager() {
+        return this.mapManager;
+    }
 
     @Override
     public MapProperties getMap() {
