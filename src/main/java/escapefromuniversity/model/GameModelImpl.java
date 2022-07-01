2a5f6425a7da59549b34419d7f133ff90e78df72
Denis Caushaj <91290254+deniscaushaj@users.noreply.github.com>
2022-07-01 12:53:45 +0200

    Fix bug

--- src/main/java/escapefromuniversity/model/GameModelImpl.java
+++ src/main/java/escapefromuniversity/model/GameModelImpl.java
@@ -95,7 +95,7 @@ public class GameModelImpl implements GameModel {
      * {@inheritDoc}
      */
     public Point2D getPositionOfID(final int id) {
-        DynamicGameObject a = this.mapManager.getGameInit().getAllDynamicGameObject().stream().filter(e -> e.getID() == id).findFirst().orElse(null);
+        DynamicGameObject a = this.mapManager.getGameInit().getAllDynamicGameObject().stream().filter(e -> e.getID() == id).findFirst().get();
         return a.getObjectPosition();
     }
 
