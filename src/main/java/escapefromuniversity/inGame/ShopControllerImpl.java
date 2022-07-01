5e4e42ab45add481fbb2c83ef650a8edd5002269
MarcoAntolini <marco.antolini2001@gmail.com>
2022-07-01 18:22:41 +0200

    implement new methods and fix bugs

--- src/main/java/escapefromuniversity/inGame/ShopControllerImpl.java
+++ src/main/java/escapefromuniversity/inGame/ShopControllerImpl.java
@@ -4,19 +4,17 @@ import escapefromuniversity.model.GameModel;
 import escapefromuniversity.model.gameObject.player.Player;
 import escapefromuniversity.model.shop.Items;
 
-import java.awt.event.MouseEvent;
+import java.awt.event.ActionEvent;
 import java.awt.event.MouseListener;
-
-import static escapefromuniversity.inGame.ShopViewImpl.*;
 
 /**
  * The implementation of {@link ShopController} interface that implements also {@link MouseListener} to add a listener to the buttons.
  */
-public class ShopControllerImpl implements ShopController, MouseListener {
+public class ShopControllerImpl implements ShopController {
 
-    private ShopView shopView;
+    private final ShopView shopView;
     private final GameController gameController;
-    private Items itemType;
+    private final Items items;
     private final Player player;
     private boolean isActive;
     private static final int resetHealthCost = 6;
@@ -34,6 +32,7 @@ public class ShopControllerImpl implements ShopController, MouseListener {
         this.player = gameModel.getPlayer();
         this.isActive = false;
         this.shopView = new ShopViewImpl(this);
+        this.items = new Items();
     }
 
     /**
@@ -42,22 +41,10 @@ public class ShopControllerImpl implements ShopController, MouseListener {
     @Override
     public void startView() {
         if(!this.isActive) {
-            //this.shopView = new ShopViewImpl(this);
             this.isActive = true;
             this.shopView.startView();
-            this.initializeButtons();
             this.checkButtonsAvailability();
-            //this.shopView.startView();
         }
-    }
-
-    /* Initializes the shop screen buttons with their mouse listeners. */
-    private void initializeButtons() {
-        this.shopView.getExit().addMouseListener(this);
-        this.shopView.getBuyLife().addMouseListener(this);
-        this.shopView.getBuyArmor().addMouseListener(this);
-        this.shopView.getBuyDamage().addMouseListener(this);
-        this.shopView.getBuyDamage().addMouseListener(this);
     }
 
     /**
@@ -70,101 +57,44 @@ public class ShopControllerImpl implements ShopController, MouseListener {
         this.isActive = false;
     }
 
-    /* Buys an item from the shop. */
-    private void buyItem() {
-        switch(this.itemType) {
-            case RESET_HEALTH:
-                this.itemType.resetHealth(this.player);
-                this.player.setCredits(this.player.getCredits() - resetHealthCost);
-                break;
-            case INCREASE_ARMOR:
-                this.itemType.increaseArmor(this.player);
-                this.player.setCredits(this.player.getCredits() - increaseArmorCost);
-                break;
-            case INCREASE_DAMAGE:
-                this.itemType.increaseDamage(this.player);
-                this.player.setCredits(this.player.getCredits() - increaseDamageCost);
-                break;
-            case DOUBLE_CHANCE:
-                this.itemType.doubleChance(this.player);
-                this.player.setCredits(this.player.getCredits() - doubleChanceCost);
-                break;
-            default: {
-            }
-        }
-        this.checkButtonsAvailability();
-    }
-
-    /**
-     * {@inheritDoc}
-     */
     @Override
-    public void mouseClicked(MouseEvent e) {
-        var source = e.getSource();
-        if (this.shopView.getExit().equals(source)) {
-            this.closeShop();
-        } else if (this.shopView.getBuyLife().equals(source)) {
-            this.itemType = Items.RESET_HEALTH;
-        } else if (this.shopView.getBuyArmor().equals(source)) {
-            this.itemType = Items.INCREASE_ARMOR;
-        } else if (this.shopView.getBuyDamage().equals(source)) {
-            this.itemType = Items.INCREASE_DAMAGE;
-        } else if (this.shopView.getBuyChance().equals(source)) {
-            this.itemType = Items.DOUBLE_CHANCE;
-        }
-        this.buyItem();
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void mousePressed(MouseEvent e) {}
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void mouseReleased(MouseEvent e) {}
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void mouseEntered(MouseEvent e) {
+    public void buyItem(ActionEvent e) {
         var source = e.getSource();
         if (this.shopView.getBuyLife().equals(source)) {
-            this.shopView.setItemInfo(buyLifeInfo);
+            this.items.resetHealth(this.player);
+            this.player.decreaseCredits(resetHealthCost);
         } else if (this.shopView.getBuyArmor().equals(source)) {
-            this.shopView.setItemInfo(buyArmorInfo);
+            this.items.increaseArmor(this.player);
+            this.player.decreaseCredits(increaseArmorCost);
         } else if (this.shopView.getBuyDamage().equals(source)) {
-            this.shopView.setItemInfo(buyDamageInfo);
+            this.items.increaseDamage(this.player);
+            this.player.decreaseCredits(increaseDamageCost);
         } else if (this.shopView.getBuyChance().equals(source)) {
-            this.shopView.setItemInfo(buyChanceInfo);
+            this.items.doubleChance(this.player);
+            this.player.decreaseCredits(doubleChanceCost);
         }
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void mouseExited(MouseEvent e) {
-        this.shopView.setItemInfo("");
+        this.checkButtonsAvailability();
+        this.shopView.setCreditsCounter();
     }
 
     /* Checks if the items can be bought and sets them not clickable in case they cannot. */
     private void checkButtonsAvailability() {
-        if(!this.itemType.increaseDamage(this.player) && this.player.getCredits() < increaseDamageCost) {
+        if(!this.items.increaseDamage(this.player) || this.player.getCredits() < increaseDamageCost) {
             this.shopView.setButtonNotClickable(this.shopView.getBuyDamage());
         }
-        if(!this.itemType.increaseArmor(this.player) && this.player.getCredits() < increaseArmorCost) {
+        if(!this.items.increaseArmor(this.player) || this.player.getCredits() < increaseArmorCost) {
             this.shopView.setButtonNotClickable(this.shopView.getBuyArmor());
         }
-        if(!this.itemType.resetHealth(this.player) && this.player.getCredits() < resetHealthCost) {
+        if(!this.items.resetHealth(this.player) || this.player.getCredits() < resetHealthCost) {
             this.shopView.setButtonNotClickable(this.shopView.getBuyLife());
         }
-        if(this.player.hasBonusQuiz() && this.player.getCredits() < doubleChanceCost) {
+        if(this.player.hasBonusQuiz() || this.player.getCredits() < doubleChanceCost) {
             this.shopView.setButtonNotClickable(this.shopView.getBuyChance());
         }
+    }
+
+    @Override
+    public Player getPlayer() {
+        return this.player;
     }
 }
